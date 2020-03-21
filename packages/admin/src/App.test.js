b66f8c3dee2211dad27d1c0945bafdab8f6263d3
Nícholas Oliveira <nicholas.andre@10up.com>
2020-03-20 21:55:28 -0300

    Updating eslint config

--- packages/admin/src/App.test.js
+++ packages/admin/src/App.test.js
@@ -2,13 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-// TODO: setup eslint for jest/tests
-
-// eslint-disable-next-line
 test('renders learn react link', () => {
 	const { getByText } = render(<App />);
 	const linkElement = getByText(/learn react/i);
 
-	// eslint-disable-next-line
 	expect(linkElement).toBeInTheDocument();
 });
