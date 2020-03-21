0682d73d675147982ee90cd06d9700c6d1c154e5
Nícholas Oliveira <nicholas.andre@10up.com>
2020-03-21 01:16:51 -0300

    Scaffold the API with Adonis.js

---
+++ packages/api/app/Models/User.js
@@ -0,0 +1,38 @@
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model');
+
+/** @type {import('@adonisjs/framework/src/Hash')} */
+const Hash = use('Hash');
+
+class User extends Model {
+	static boot() {
+		super.boot();
+
+		/**
+		 * A hook to hash the user password before saving
+		 * it to the database.
+		 */
+		this.addHook('beforeSave', async (userInstance) => {
+			if (userInstance.dirty.password) {
+				// eslint-disable-next-line no-param-reassign
+				userInstance.password = await Hash.make(userInstance.password);
+			}
+		});
+	}
+
+	/**
+	 * A relationship on tokens is required for auth to
+	 * work. Since features like `refreshTokens` or
+	 * `rememberToken` will be saved inside the
+	 * tokens table.
+	 *
+	 * @function tokens
+	 *
+	 * @returns {object}
+	 */
+	tokens() {
+		return this.hasMany('App/Models/Token');
+	}
+}
+
+module.exports = User;
---
+++ packages/api/start/routes.js
@@ -0,0 +1,19 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route');
+
+Route.get('/', () => {
+	return { greeting: 'Olá plataforma sabia!' };
+});
