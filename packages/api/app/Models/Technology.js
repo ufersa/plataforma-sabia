604d4d3aadcc596951a6ec58cd62801756585dfa
Nícholas Oliveira <nicholas.andre@10up.com>
2020-03-26 09:01:12 -0300

    Hooking Technology model with Algolia

--- packages/api/app/Models/Technology.js
+++ packages/api/app/Models/Technology.js
@@ -1,12 +1,21 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model');
+const Config = use('Adonis/Src/Config');
+const algoliasearch = use('App/AlgoliaSearch');
 
 class Technology extends Model {
 	static boot() {
 		super.boot();
+		const algoliaConfig = Config.get('algolia');
+		const indexObject = algoliasearch.initIndex(algoliaConfig.indexName);
+
 		// eslint-disable-next-line no-unused-vars
 		this.addHook('afterSave', async (technology) => {
-			// save/update algolia
+			indexObject.saveObject(technology.toJSON());
+		});
+
+		this.addHook('afterDelete', async (technology) => {
+			indexObject.deleteObjects(technology.objectID);
 		});
 	}
 
