1f6c17a73f623a791f538a44b67f6bc35eb64b9d
NaylsonFerreira <naylsonfsa@gmail.com>
2020-04-04 13:25:29 -0300

    Add auth to admin

--- packages/admin/src/App.js
+++ packages/admin/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import authProvider from './authProvider';
+import dataProvider from './dataProvider';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+import LoginPage from './LoginPage';
+
 const App = () => (
-	<Admin dataProvider={dataProvider}>
+	<Admin loginPage={LoginPage} authProvider={authProvider} dataProvider={dataProvider}>
 		<Resource name="users" list={ListGuesser} />
+		<Resource name="posts" list={ListGuesser} />
 	</Admin>
 );
 export default App;
