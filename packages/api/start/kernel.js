3f3e8355b66f31e0a906761bbb4130a8c8865da3
NaylsonFerreira <naylsonfsa@gmail.com>
2020-04-04 10:48:28 -0300

    Add CORS to Adonis

--- packages/api/start/kernel.js
+++ packages/api/start/kernel.js
@@ -48,10 +48,7 @@ const namedMiddleware = {
 | control over request lifecycle.
 |
 */
-const serverMiddleware = [
-	// 'Adonis/Middleware/Static',
-	'Adonis/Middleware/Cors',
-];
+const serverMiddleware = ['Adonis/Middleware/Cors'];
 
 Server.registerGlobal(globalMiddleware)
 	.registerNamed(namedMiddleware)
