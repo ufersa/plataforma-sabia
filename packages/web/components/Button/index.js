59209f89eff6d114809da4c7b349096a52a82d13
Nícholas Oliveira <nicholas.andre@10up.com>
2020-04-19 15:31:12 -0300

    Initial storybook implementatino

--- packages/web/components/Button/index.js
+++ packages/web/components/Button/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { useTheme } from 'styled-components';
 import StyledButton from './styles';
 
-const Button = ({ children, onClick, type, bgColor, color }) => {
+const Button = ({ children, onClick, variant, type }) => {
+	const { colors } = useTheme();
+
+	const bgColor = variant === 'primary' ? colors.primary : colors.black;
+	const color = variant === 'primary' ? colors.white : colors.white;
+
 	return (
 		<StyledButton onClick={onClick} type={type} bgColor={bgColor} color={color}>
 			{children}
@@ -13,14 +19,15 @@ const Button = ({ children, onClick, type, bgColor, color }) => {
 
 Button.propTypes = {
 	children: PropTypes.string.isRequired,
-	onClick: PropTypes.func.isRequired,
-	bgColor: PropTypes.string.isRequired,
-	color: PropTypes.string.isRequired,
+	variant: PropTypes.oneOf(['primary', 'secondary']),
+	onClick: PropTypes.func,
 	type: PropTypes.string,
 };
 
 Button.defaultProps = {
 	type: 'button',
+	variant: 'primary',
+	onClick: () => {},
 };
 
 export default Button;
---
+++ packages/web/stories/Button.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Button from '../components/Button';
+
+export default {
+	title: 'Button',
+	component: Button,
+};
+
+export const PrimaryVariant = () => <Button onClick={action('clicked')}>I am a button.</Button>;
+export const SecondaryVariant = () => (
+	<Button onClick={action('clicked')} variant="secondary">
+		I am a button.
+	</Button>
+);
