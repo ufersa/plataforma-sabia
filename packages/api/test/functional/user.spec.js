44194e988ef021d2659af85619002e6dfdba1452
Nícholas Oliveira <nicholas.andre@10up.com>
2020-04-26 18:56:24 -0300

    Adding api tests for user/me

---
+++ packages/api/test/functional/user.spec.js
@@ -0,0 +1,32 @@
+const { test, trait } = use('Test/Suite')('User');
+
+const User = use('App/Models/User');
+
+trait('Test/ApiClient');
+trait('Auth/Client');
+trait('DatabaseTransactions');
+
+const user = {
+	username: 'sabiatestinguser',
+	email: 'sabiatestingemail@gmail.com',
+	password: '123123',
+};
+
+test('/user/me endpoint works', async ({ client }) => {
+	const loggeduser = await User.create(user);
+
+	const response = await client
+		.get('/user/me')
+		.loginVia(loggeduser, 'jwt')
+		.end();
+
+	loggeduser.password = '';
+	response.assertStatus(200);
+	response.assertJSONSubset(loggeduser.toJSON());
+});
+
+test('/user/me errors out if no jwt token provided', async ({ client }) => {
+	const response = await client.get('/user/me').end();
+
+	response.assertStatus(401);
+});
