d5f9832b7e53f354906ce4e94d0e3059347b8d52
Nícholas Oliveira <nicholas.andre@10up.com>
2020-04-27 01:15:01 -0300

    Increasing failure threhsold

--- packages/web/tests/integration/setupTests.js
+++ packages/web/tests/integration/setupTests.js
@@ -1,8 +1,9 @@
 const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
 
-const customConfig = { threshold: 0.03 };
+const customConfig = { threshold: 0.01 };
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
 	customDiffConfig: customConfig,
+	failureThreshold: 0.03,
 });
 
 expect.extend({ toMatchImageSnapshot });
