8993ca8e21d6162522029989ecdc93493bc03ae6
Nícholas Oliveira <nicholas.andre@10up.com>
2020-05-02 13:05:00 -0300

    Adding a delay before taking the screenshot of all tests

--- packages/web/tests/visual/storyshoots.test.js
+++ packages/web/tests/visual/storyshoots.test.js
@@ -5,6 +5,7 @@ initStoryshots({
 	suite: 'Visual Regression',
 	test: imageSnapshot({
 		storybookUrl: 'http://localhost:9009',
+		beforeScreenshot: () => new Promise((resolve) => setTimeout(() => resolve(), 300)),
 		getScreenshotOptions: () => ({
 			encoding: 'base64',
 		}),
