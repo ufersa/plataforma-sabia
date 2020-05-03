79c45ef8e0c9fa5430feb49c6b874649c6dc9cfc
Alexandre <alexandre.adames@gmail.com>
2020-05-03 12:57:13 -0300

    Migrations => Taxonomy, Term and Technology_Term

---
+++ packages/api/database/migrations/1588520608067_term_schema.js
@@ -0,0 +1,28 @@
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class TermsSchema extends Schema {
+	up() {
+		this.create('terms', (table) => {
+			table.increments();
+			table.string('term').notNullable();
+			table
+				.integer('parent_id')
+				.unsigned()
+				.references('id')
+				.inTable('terms');
+			table
+				.integer('taxonomy_id')
+				.unsigned()
+				.references('id')
+				.inTable('taxonomies');
+			table.timestamps();
+		});
+	}
+
+	down() {
+		this.drop('terms');
+	}
+}
+
+module.exports = TermsSchema;
