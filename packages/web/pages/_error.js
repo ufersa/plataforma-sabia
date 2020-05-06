b56961df8ce8ee1d37ae9b8f4de9c99e36e790e8
Luiz Claudio <luizcns18@gmail.com>
2020-05-06 19:42:28 -0300

    Stylize error page

--- packages/web/pages/_error.js
+++ packages/web/pages/_error.js
@@ -1,14 +1,78 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { Link } from '../components/Link';
 
 const Error = ({ statusCode }) => {
 	const { t } = useTranslation(['error']);
-	if (statusCode === 404) {
-		return <h1>404 - {t('notFoundPageError')}</h1>;
-	}
-	return <h1>{statusCode ? t('serverError', { statusCode }) : t('clientError')}</h1>;
+	return (
+		<Container>
+			{statusCode ? (
+				<>
+					<h1>{statusCode}</h1>
+					<h2>
+						{statusCode === 404
+							? t('notFoundPageError')
+							: t('serverError', { statusCode })}
+					</h2>
+				</>
+			) : (
+				<h1>{t('clientError')}</h1>
+			)}
+			<Link href="/">
+				<AiOutlineArrowLeft /> {t('backButton')}
+			</Link>
+		</Container>
+	);
 };
+
+Error.propTypes = {
+	statusCode: PropTypes.number.isRequired,
+};
+
+const Container = styled.div`
+	height: 100vh;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background-color: ${({ theme }) => theme.colors.gray98};
+	text-align: center;
+
+	h1 {
+		color: ${({ theme }) => theme.colors.darkGray};
+		font-size: 3.6rem;
+		margin-bottom: 1.5rem;
+	}
+
+	h2 {
+		font-size: 2rem;
+	}
+
+	a {
+		margin-top: 6rem;
+		padding: 1.5rem 3rem;
+		display: flex;
+		align-items: center;
+		background-color: ${({ theme }) => theme.colors.primary};
+		color: ${({ theme }) => theme.colors.white};
+		text-transform: uppercase;
+		border-radius: ${({ theme }) => theme.metrics.baseRadius}rem;
+
+		svg {
+			margin-right: 1rem;
+		}
+
+		:hover {
+			background-color: ${({ theme }) => theme.colors.darkGray};
+			color: ${({ theme }) => theme.colors.white};
+			cursor: pointer;
+		}
+	}
+`;
 
 Error.getInitialProps = async ({ res, err }) => {
 	let statusCode;
@@ -20,10 +84,6 @@ Error.getInitialProps = async ({ res, err }) => {
 		statusCode = 404;
 	}
 	return { statusCode, namespacesRequired: ['error'] };
-};
-
-Error.propTypes = {
-	statusCode: PropTypes.number.isRequired,
 };
 
 export default Error;
