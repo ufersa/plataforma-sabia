f4335e49974f53c5c22f6f576efe4d238dd4f084
NaylsonFerreira <naylsonfsa@gmail.com>
2020-05-12 00:07:47 -0300

    Added ReCaptcha in api and admin

--- packages/admin/src/components/Auth/index.js
+++ packages/admin/src/components/Auth/index.js
@@ -1,5 +1,3 @@
-import LoginPage from './LoginPage';
-import ForgotPassword from './ForgotPassword';
-import ResetPassword from './ResetPassword';
-
-export { LoginPage, ForgotPassword, ResetPassword };
+export { default as LoginPage } from './LoginPage';
+export { default as ForgotPassword } from './ForgotPassword';
+export { default as ResetPassword } from './ResetPassword';
