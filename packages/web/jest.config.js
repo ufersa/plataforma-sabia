3bff425ed9c5c465a242d101d56ddc01e0c5618c
Nícholas Oliveira <nicholas.andre@10up.com>
2020-05-30 02:06:37 -0300

    watch-typeahead plugin

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
 	testPathIgnorePatterns: ['/node_modules/', '/tests/visual/'],
 	collectCoverageFrom: ['**/*.js'],
 	coveragePathIgnorePatterns: ['/node_modules/', 'config.js', 'server.js'],
+	watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
 };
