33f757be6daa5af41e65267145419120118b4c84
Nícholas Oliveira <nicholas.andre@10up.com>
2020-06-11 22:08:57 -0300

    Fixing linting errors

--- packages/web/pages/_error.js
+++ packages/web/pages/_error.js
@@ -77,9 +77,9 @@ const Container = styled.div`
 Error.getInitialProps = async ({ res, err }) => {
 	let statusCode;
 	if (res) {
-		({ statusCode } = res.statusCode);
+		({ statusCode } = res);
 	} else if (err) {
-		({ statusCode } = err.statusCode);
+		({ statusCode } = err);
 	} else {
 		statusCode = 404;
 	}
