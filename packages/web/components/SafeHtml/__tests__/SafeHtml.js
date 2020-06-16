5e1dfffe109694cdfd727237c48d65f347d6e5b6
mateus4k <mateus4k@protonmail.ch>
2020-06-16 00:04:57 -0300

    test(safeHTML): initial cases

---
+++ packages/web/components/SafeHtml/__tests__/SafeHtml.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { render } from 'test-utils';
+import { SafeHtml } from '..';
+
+const renderText = 'hello';
+
+describe('SafeHtml component', () => {
+	it('should sanitize and parse a dirty html string', () => {
+		const dirtyHTML = `<TABLE><tr><td>${renderText}</tr></TABL>`;
+
+		const { container } = render(<SafeHtml html={dirtyHTML} />);
+
+		// expect(container).toMatchSnapshot();
+		expect(container.textContent).toStrictEqual(renderText);
+	});
+
+	it('should allow passed tags and attributes', () => {
+		const dirtyHTML = `<p style="color:red;">${renderText}<iframe//src=jAva&Tab;script:alert(3)></p>`;
+
+		const config = {
+			ALLOWED_TAGS: ['p'],
+			ALLOWED_ATTR: ['style'],
+		};
+
+		const { container } = render(
+			<SafeHtml
+				html={dirtyHTML}
+				allowedTags={config.ALLOWED_TAGS}
+				allowedAttrs={config.ALLOWED_ATTR}
+			/>,
+		);
+
+		expect(container).toMatchInlineSnapshot(`
+		<div>
+		  <p
+		    style="color: red;"
+		  >
+		    ${renderText}
+		  </p>
+		</div>
+	`);
+	});
+
+	it('should not allow unpassed tags and attributes', () => {
+		const dirtyHTML = `<section><img src="${renderText}" id="xss" onerror=alert(1)//></section>`;
+
+		const config = {
+			ALLOWED_TAGS: ['img'],
+			ALLOWED_ATTR: ['src'],
+		};
+
+		const { container } = render(
+			<SafeHtml
+				html={dirtyHTML}
+				allowedTags={config.ALLOWED_TAGS}
+				allowedAttrs={config.ALLOWED_ATTR}
+			/>,
+		);
+
+		expect(container).toMatchInlineSnapshot(`
+        <div>
+          <img
+            src="${renderText}"
+          />
+        </div>
+	    `);
+	});
+});
