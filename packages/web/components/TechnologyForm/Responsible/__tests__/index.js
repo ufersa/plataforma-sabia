a5e464d30b1f96785eaf37ab195334c5fd940af0
Nícholas Oliveira <nicholas.andre@10up.com>
2020-06-21 14:18:40 -0300

    Fixing tests, and disabling jest-styled-components

--- packages/web/components/TechnologyForm/Responsible/__tests__/index.js
+++ packages/web/components/TechnologyForm/Responsible/__tests__/index.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import { render, fireEvent, screen } from 'test-utils';
 import Responsible from '..';
+import { Form } from '../../../Form';
+
+const onSubmit = jest.fn(() => {});
 
 jest.mock('react-icons/fa', () => {
 	return {
@@ -15,92 +18,35 @@ jest.mock('react-icons/fa', () => {
 	};
 });
 
-test.skip('it renders the form with one row', () => {
+test('it renders the form with one row', () => {
 	const { container } = render(
-		<Responsible
-			form={{
-				errors: {},
-				register: jest.fn(),
-				control: {},
-			}}
-		/>,
+		<Form onSubmit={onSubmit}>
+			<Responsible />
+		</Form>,
 	);
 
 	expect(container).toMatchSnapshot();
 });
 
-test.skip('it increase the number of rows when clicking in add button', () => {
+test('it increase the number of rows when clicking in add button', () => {
 	render(
-		<Responsible
-			form={{
-				errors: {},
-				register: jest.fn(),
-			}}
-		/>,
+		<Form onSubmit={onSubmit}>
+			<Responsible />
+		</Form>,
 	);
 
 	const buttons = screen.getAllByRole('button');
 
 	fireEvent.click(buttons[1]);
 
-	expect(buttons[1]).toMatchInlineSnapshot(`
-		.c0 {
-		  background-color: hsla(195,86%,64%);
-		  color: hsla(0,0%,100%);
-		  border-radius: 100%;
-		  height: 100%;
-		  border: none;
-		  font-size: 2rem;
-		  text-transform: uppercase;
-		  text-align: center;
-		  -webkit-text-decoration: none;
-		  text-decoration: none;
-		  display: inline-block;
-		  padding: 1rem !important;
-		  float: right;
-		  display: -webkit-box;
-		  display: -webkit-flex;
-		  display: -ms-flexbox;
-		  display: flex;
-		}
-
-		.c0:hover {
-		  opacity: 0.8;
-		}
-
-		@media (max-width:450px) {
-
-		}
-
-		@media (max-width:450px) {
-
-		}
-
-		@media (max-width:450px) {
-
-		}
-
-		<button
-		  class="c0"
-		  color="hsla(0, 0%, 100%)"
-		  title=""
-		  type="button"
-		>
-		  <svg>
-		    FaPlus mocked
-		  </svg>
-		</button>
-	`);
+	expect(buttons[1]).toMatchSnapshot();
 });
 
-test.skip('it decrease the number of rows when clicking in remove button', () => {
+test('it decrease the number of rows when clicking in remove button', () => {
 	const { container } = render(
-		<Responsible
-			form={{
-				errors: {},
-				register: jest.fn(),
-			}}
-		/>,
+		<Form onSubmit={onSubmit}>
+			<Responsible />,
+		</Form>,
 	);
 
 	const buttons = screen.getAllByRole('button');
@@ -114,14 +60,11 @@ test.skip('it decrease the number of rows when clicking in remove button', () =>
 	expect(container).toMatchSnapshot();
 });
 
-test.skip('it should not remove the row when only one is available', () => {
+test('it should not remove the row when only one is available', () => {
 	const { container } = render(
-		<Responsible
-			form={{
-				errors: {},
-				register: jest.fn(),
-			}}
-		/>,
+		<Form onSubmit={onSubmit}>
+			<Responsible />
+		</Form>,
 	);
 
 	const buttons = screen.getAllByRole('button');
