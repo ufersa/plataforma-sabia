c68305c3875d01db5895fa3bf625493baa2ff176
mateus4k <mateus4k@protonmail.ch>
2020-06-27 01:33:32 -0300

    test(request): form submit

--- cypress/e2e/web/resetPassword.js
+++ cypress/e2e/web/resetPassword.js
@@ -2,14 +2,16 @@ const data = {
 	email: 'sabiatestinge2e@gmail.com',
 	password: 'sabiatesting',
 	pages: {
-		request: '/',
-		reset: '/auth/reset-password',
+		home: '/',
+		reset:
+			'/auth/reset-password?token=3ef578fb424624825af49a0dbf336418TdXsguo1xUlplWqXFaSmLNrUGOVS/0mWO0DKuCBSb10=',
+		resetWithoutToken: '/auth/reset-password',
 	},
 };
 
 describe('reset password', () => {
 	it('can request password reset ', () => {
-		cy.visit(data.pages.request);
+		cy.visit(data.pages.home);
 		cy.findByText(/^(entrar|sign in)$/i).click();
 		cy.findByText(/^(esqueci minha senha|forgot the password)$/i).click();
 		cy.get('form input[name=email]').type(data.email);
@@ -20,5 +22,14 @@ describe('reset password', () => {
 		cy.findByText(
 			/^(o link de recuperação de senha foi enviado para o seu e-mail|the password recovery link has been sent to your email)/i,
 		).should('exist');
+	});
+
+	it('can submit the reset password form', () => {
+		cy.visit(data.pages.reset);
+		cy.findByText(/^(recuperação de senha|password recovery)$/i);
+		cy.get('#password').type(data.password);
+		cy.get('form button[type=submit]')
+			.click()
+			.should('be.visible');
 	});
 });
