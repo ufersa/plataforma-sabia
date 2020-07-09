2f866829eaa9afd364f795c68391d69c69050391
Marcus Mota <marcushcsm@gmail.com>
2020-07-09 19:54:20 +0100

    remove SelectDefault from the Form root file

--- packages/web/components/Form/index.js
+++ packages/web/components/Form/index.js
@@ -2,7 +2,6 @@ export { default as InputField } from './InputField';
 export { default as SwitchField } from './SwitchField';
 export { default as TextField } from './TextField';
 export { default as SelectField } from './SelectField';
-export { default as SelectDefault } from './SelectDefault';
 export { default as CheckBoxField } from './CheckBoxField';
 export { default as Watcher } from './Watcher';
 export * from './Form';
