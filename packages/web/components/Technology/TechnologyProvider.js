74c5748724b874fe40bed79a2e401194261c9c9f
mateus4k <mateus4k@protonmail.ch>
2020-07-11 21:00:59 -0300

    feat(tech): create provider

---
+++ packages/web/components/Technology/TechnologyProvider.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TechnologyContext from './TechnologyContext';
+
+export const TechnologyProvider = ({ children, technology }) => {
+	return (
+		<TechnologyContext.Provider value={{ technology }}>{children}</TechnologyContext.Provider>
+	);
+};
+
+TechnologyProvider.propTypes = {
+	children: PropTypes.node.isRequired,
+	technology: PropTypes.shape({}),
+};
+
+TechnologyProvider.defaultProps = {
+	technology: {},
+};
+
+export default TechnologyProvider;
