c055697a63ab8c62d701d1b62dd2ce112f702572
Marcus Mota <marcushcsm@gmail.com>
2020-07-12 14:05:12 +0100

    add jsdoc to the watcher component

--- packages/web/components/Form/Watcher.js
+++ packages/web/components/Form/Watcher.js
@@ -1,7 +1,17 @@
 import PropTypes from 'prop-types';
 
+/**
+ * Output render function with the property watched as parameter
+ *
+ * @param {object} props Component Props
+ * @param {object} props.form react-hook-form
+ * @param {string} props.property property to be watched from the form
+ * @param {number} props.index index of the element to be watched if the property is an array
+ * @param {string} props.render function to be rendered when the watched element changes
+ * @returns {React.Component} render function from props
+ */
 const Watcher = ({ form, property, index, render }) => {
-	const element = form.watch(typeof index === 'undefined' ? property : `${property}[${index}]`);
+	const element = form.watch(index === null ? property : `${property}[${index}]`);
 	return render(element);
 };
 
@@ -14,6 +24,8 @@ Watcher.propTypes = {
 	}).isRequired,
 };
 
-Watcher.defaultProps = {};
+Watcher.defaultProps = {
+	index: null,
+};
 
 export default Watcher;
