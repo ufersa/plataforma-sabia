648a23ad8abfc7c677cb7410eb837b6c1bd4755e
Luiz Claudio <luizcns18@gmail.com>
2020-07-17 19:04:59 -0300

    Move tests location

--- packages/web/__tests__/pages/index.js
+++ packages/web/__tests__/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'test-utils';
-import MyAccount from '../index';
+import MyAccount from '../../pages/user/my-account';
 
 test('it renders MyAccount main page correctly', () => {
 	const { container } = render(<MyAccount />);
