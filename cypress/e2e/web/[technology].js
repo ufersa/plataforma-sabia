a7c6b9c98c253f9aae6c1bf195f9e68868475841
mateus4k <mateus4k@protonmail.ch>
2020-07-17 23:34:42 -0300

    test(tech): making assertions

--- cypress/e2e/web/[technology].js
+++ cypress/e2e/web/[technology].js
@@ -2,16 +2,19 @@ describe('technology details', () => {
 	it('should list details', () => {
 		cy.request('GET', 'http://localhost:3333/technologies', {
 			perPage: 1,
-		}).then((technologies) => {
-			const technology = technologies.body[0];
+		}).then((response) => {
+			const technology = response.body[0];
 
-			cy.request('GET', `http://localhost:3333/technologies/${technology.slug}`).then(
-				(response) => {
-					cy.expect(response.status).to.equal(200);
+			cy.expect(response.status).to.equal(200);
 
-					cy.visit(`/${technology.slug}`);
-				},
-			);
+			cy.visit(`/${technology.slug}`);
+
+			cy.findAllByText(new RegExp(technology.title, 'i')).should('exist');
+			cy.findAllByText(new RegExp(technology.description, 'im')).should('exist');
+			cy.get('[data-testid=image]')
+				.should('be.visible')
+				.should('have.attr', 'src')
+				.should('include', technology.thumbnail);
 		});
 	});
 });
