3a2b5d8659b345aa88f43f1b735cbedcb2a2067b
mateus4k <mateus4k@protonmail.ch>
2020-07-25 16:31:06 -0300

    refactor(tabs): make props more readable

--- packages/web/components/Tab/styles.js
+++ packages/web/components/Tab/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import * as ReactTabs from 'react-tabs';
 
 export const Tabs = styled(ReactTabs.Tabs)`
@@ -6,52 +6,51 @@ export const Tabs = styled(ReactTabs.Tabs)`
 `;
 
 export const Tab = styled(ReactTabs.Tab)`
-	display: inline-block;
-	bottom: -1px;
-	position: relative;
-	list-style: none;
-	font-size: 1.4rem;
-	line-height: 17px;
-	text-align: center;
-	padding: 1.6rem 3.2rem;
-	text-transform: uppercase;
-	border: 0.5px solid transparent;
-	border-radius: 0;
+	${({ selected, disabled, theme: { colors } }) => css`
+		display: inline-block;
+		bottom: -1px;
+		position: relative;
+		list-style: none;
+		font-size: 1.4rem;
+		line-height: 17px;
+		text-align: center;
+		padding: 1.6rem 3.2rem;
+		text-transform: uppercase;
+		border: 0.5px solid transparent;
+		border-radius: 0;
+		color: ${selected ? colors.lightGray : colors.lightGray2};
 
-	color: ${({ selected, theme }) =>
-		selected ? theme.colors.lightGray : theme.colors.lightGray2};
+		cursor: ${disabled ? 'default' : 'pointer'};
 
-	cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+		background: ${selected ? colors.white : colors.lightGray4};
 
-	background: ${({ selected, theme }) =>
-		selected ? theme.colors.white : theme.colors.lightGray4};
+		border-left-color: ${colors.lightGray3};
+		border-right-color: ${colors.lightGray3};
 
-	border-left-color: ${({ theme }) => theme.colors.lightGray3};
-	border-right-color: ${({ theme }) => theme.colors.lightGray3};
-
-	&:first-child {
-		border-left-color: transparent;
-	}
-
-	&:last-child {
-		border-right-color: transparent;
-	}
-
-	&:focus {
-		box-shadow: 0 0 5px ${({ theme }) => theme.colors.darkWhite};
-		border-color: ${({ theme }) => theme.colors.darkWhite};
-		outline: none;
-
-		&:after {
-			content: '';
-			position: absolute;
-			height: 5px;
-			left: -4px;
-			right: -4px;
-			bottom: -5px;
-			background: ${({ theme }) => theme.colors.white};
+		&:first-child {
+			border-left-color: transparent;
 		}
-	}
+
+		&:last-child {
+			border-right-color: transparent;
+		}
+
+		&:focus {
+			box-shadow: 0 0 5px ${colors.darkWhite};
+			border-color: ${colors.darkWhite};
+			outline: none;
+
+			&:after {
+				content: '';
+				position: absolute;
+				height: 5px;
+				left: -4px;
+				right: -4px;
+				bottom: -5px;
+				background: ${colors.white};
+			}
+		}
+	`}
 `;
 
 export const TabPanel = styled(ReactTabs.TabPanel)`
