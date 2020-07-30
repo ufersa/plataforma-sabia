53f24b6b9de937203b57b5aa8e00162c7f1a9d93
mateus4k <mateus4k@protonmail.ch>
2020-07-30 19:53:59 -0300

    test(tech): redirect to 404

--- cypress/e2e/web/technologyDetails.js
+++ cypress/e2e/web/technologyDetails.js
@@ -17,4 +17,14 @@ describe('technology details', () => {
 				.should('include', technology.thumbnail);
 		});
 	});
+
+	it('should redirect to the error page when the technology is not found', () => {
+		const unknownTechnology = 'a';
+
+		cy.visit(`/t/${unknownTechnology}`, {
+			failOnStatusCode: false,
+		});
+		cy.url().should('match', /_error.js/);
+		cy.findAllByText('404').should('exist');
+	});
 });
