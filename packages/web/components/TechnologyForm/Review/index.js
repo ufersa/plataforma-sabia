8ccd1f4b5f1624bc8755dba021e4ea80aea4bb66
mateus4k <mateus4k@protonmail.ch>
2020-08-16 15:56:36 -0300

    feat(review): get funding label by value

--- packages/web/components/TechnologyForm/Review/index.js
+++ packages/web/components/TechnologyForm/Review/index.js
@@ -14,6 +14,49 @@ const Review = ({ form }) => {
 		title: '',
 	};
 
+	const fundingData = {
+		types: [
+			{
+				value: 'public',
+				label: 'Público',
+			},
+			{
+				value: 'private',
+				label: 'Privado',
+			},
+			{
+				value: 'collective',
+				label: 'Coletivo',
+			},
+		],
+		status: [
+			{
+				value: 'not_acquired',
+				label: 'Não adquirido',
+			},
+			{
+				value: 'acquiring',
+				label: 'Em aquisição',
+			},
+			{
+				value: 'acquired',
+				label: 'Já adquirido',
+			},
+		],
+	};
+
+	const getFundingLabelByValue = (scope, value) => {
+		const keys = Object.keys(fundingData);
+
+		if (!scope || !keys.some((key) => key === scope)) {
+			return value;
+		}
+
+		const funding = fundingData[scope].find((data) => data.value === value);
+
+		return funding?.label || value;
+	};
+
 	return (
 		<Wrapper>
 			<Repeater
@@ -101,7 +144,10 @@ const Review = ({ form }) => {
 								/>
 								<TextValue
 									title="Tipo de Financiamento"
-									value={technology.technologyCosts?.funding_type}
+									value={getFundingLabelByValue(
+										'types',
+										technology.technologyCosts?.funding_type,
+									)}
 								/>
 								<TextValue
 									title="Valor do Financiamento"
@@ -109,7 +155,10 @@ const Review = ({ form }) => {
 								/>
 								<TextValue
 									title="Situação"
-									value={technology.technologyCosts?.funding_status}
+									value={getFundingLabelByValue(
+										'status',
+										technology.technologyCosts?.funding_status,
+									)}
 								/>
 							</Section>
 
