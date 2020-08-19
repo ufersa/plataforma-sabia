233113693f2f32433e1fa76ae8d772b7e61b7359
mateus4k <mateus4k@protonmail.ch>
2020-08-19 08:19:08 -0300

    fix(wizard): remove commented test

--- cypress/e2e/web/createTechnology.js
+++ cypress/e2e/web/createTechnology.js
@@ -1,19 +1,19 @@
 describe('technology form validation', () => {
-	// beforeEach(() => {
-	// 	cy.authenticate().visit('/technology/new');
-	// });
-	// it('step 1 - must fill required fields', () => {
-	// 	cy.get('input[name=title]').type('Minha tecnologia');
-	// 	cy.get('textarea[name=description]').type('Descrição da tecnologia');
-	// 	cy.findByText(/salvar e continuar/i).click();
-	// 	cy.findAllByText(/este campo é obrigatório/i).should('exist');
-	// });
-	// it('step 1 - selecting a category renders subcategories', () => {
-	// 	cy.findByText(/escolha uma categoria primeiro/i).should('exist');
-	// 	cy.select('terms.category', { exactMatch: true });
-	// 	cy.findByText(/escolha uma categoria primeiro/i).should('not.exist');
-	// 	cy.findByText(/escolha a sub categoria/i).should('exist');
-	// });
+	beforeEach(() => {
+		cy.authenticate().visit('/technology/new');
+	});
+	it('step 1 - must fill required fields', () => {
+		cy.get('input[name=title]').type('Minha tecnologia');
+		cy.get('textarea[name=description]').type('Descrição da tecnologia');
+		cy.findByText(/salvar e continuar/i).click();
+		cy.findAllByText(/este campo é obrigatório/i).should('exist');
+	});
+	it('step 1 - selecting a category renders subcategories', () => {
+		cy.findByText(/escolha uma categoria primeiro/i).should('exist');
+		cy.select('terms.category', { exactMatch: true });
+		cy.findByText(/escolha uma categoria primeiro/i).should('not.exist');
+		cy.findByText(/escolha a sub categoria/i).should('exist');
+	});
 });
 
 describe('creating/editing technology', () => {
@@ -21,10 +21,10 @@ describe('creating/editing technology', () => {
 		cy.authenticate().visit('/technology/new');
 	});
 
-	// it('redirects /technology/new if technology does not exist or does not belong to user', () => {
-	// 	cy.visit('/technology/9999/edit');
-	// 	cy.url().should('include', 'technology/new');
-	// });
+	it('redirects /technology/new if technology does not exist or does not belong to user', () => {
+		cy.visit('/technology/9999/edit');
+		cy.url().should('include', 'technology/new');
+	});
 
 	it('filling all fields creates an technology', () => {
 		cy.fixture('technology.json').then((technologyData) => {
