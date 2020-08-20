854804274486ab77dff0a016c7463b04a7fcf102
Luiz Cláudio <luizcns18@gmail.com>
2020-08-20 10:46:35 -0300

    Show owner data

--- packages/web/components/TechnologyForm/Responsible/index.js
+++ packages/web/components/TechnologyForm/Responsible/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FaPlus, FaMinus } from 'react-icons/fa';
 import styled from 'styled-components';
-import { InputField, Repeater, MaskedInputField } from '../../Form';
+import { InputField, Repeater, MaskedInputField, Help } from '../../Form';
 import { CircularButton } from '../../Button';
 import { Cell, Row } from '../../Common/Layout';
+import { useAuth } from '../../../hooks';
 
 const Wrapper = styled.div`
 	margin-bottom: 4rem;
@@ -18,72 +19,78 @@ const Responsible = ({ form }) => {
 		phone_number: '',
 		lattes_id: '',
 	};
-
+	const { user } = useAuth();
 	const dataName = 'technologyResponsibles';
 	const owner = `${dataName}.owner`;
 	const users = `${dataName}.users`;
 
 	return (
 		<Wrapper>
-			<Repeater
-				form={form}
-				name={owner}
-				title="Responsáveis Pela Tecnologia"
-				help={
-					<p>
-						Adicione o nome dos responsáveis pelas tecnologias.
-						<br /> O ID Lattes é importante para que a equipe de avaliadores possa
-						analisar os dados dos pesquisadores com mais detalhes.
-					</p>
-				}
-				childsComponent={({ item }) => {
-					return (
-						<Row data-testid="row" key={item.id}>
-							<InputField form={form} name={`${owner}.user_id`} type="hidden" />
-							<Cell col={5}>
-								<InputField
-									form={form}
-									name={`${owner}.full_name`}
-									label="Nome Completo"
-									disabled
-								/>
-							</Cell>
-							<Cell col={3}>
-								<InputField
-									form={form}
-									name={`${owner}.email`}
-									label="Email"
-									disabled
-								/>
-							</Cell>
-							<Cell col={2}>
-								<InputField
-									form={form}
-									name={`${owner}.phone_number`}
-									label="Telefone"
-									disabled
-								/>
-							</Cell>
-							<Cell col={2}>
-								<InputField
-									form={form}
-									name={`${owner}.new_lattes_id`}
-									label="ID Lattes"
-									placeholder="Somente números"
-									type="number"
-								/>
-							</Cell>
-							<InputField
-								form={form}
-								name={`${owner}.current_lattes_id`}
-								type="hidden"
-							/>
-							<Cell maxWidth={0.5} />
-						</Row>
-					);
-				}}
-			/>
-
+			<Row align="center">
+				<h3>Responsáveis Pela Tecnologia</h3>
+				<Help
+					id={owner}
+					HelpComponent={
+						<p>
+							Adicione o nome dos responsáveis pelas tecnologias.
+							<br /> O ID Lattes é importante para que a equipe de avaliadores possa
+							analisar os dados dos pesquisadores com mais detalhes.
+						</p>
+					}
+				/>
+			</Row>
+			<Row data-testid="row">
+				<InputField
+					form={form}
+					name={`${owner}.user_id`}
+					defaultValue={user.id}
+					type="hidden"
+				/>
+				<Cell col={5}>
+					<InputField
+						form={form}
+						name={`${owner}.full_name`}
+						label="Nome Completo"
+						disabled
+						defaultValue={user.full_name}
+					/>
+				</Cell>
+				<Cell col={3}>
+					<InputField
+						form={form}
+						name={`${owner}.email`}
+						label="Email"
+						disabled
+						defaultValue={user.email}
+					/>
+				</Cell>
+				<Cell col={2}>
+					<InputField
+						form={form}
+						name={`${owner}.phone_number`}
+						label="Telefone"
+						disabled
+						defaultValue={user.phone_number}
+					/>
+				</Cell>
+				<Cell col={2}>
+					<InputField
+						form={form}
+						name={`${owner}.new_lattes_id`}
+						label="ID Lattes"
+						placeholder="Somente números"
+						type="number"
+						defaultValue={user.lattes_id}
+					/>
+				</Cell>
+				<InputField
+					form={form}
+					name={`${owner}.current_lattes_id`}
+					type="hidden"
+					defaultValue={user.lattes_id}
+				/>
+				<Cell maxWidth={0.5} />
+			</Row>
 			<Repeater
 				form={form}
 				name={users}
@@ -119,7 +126,7 @@ const Responsible = ({ form }) => {
 										placeholder="(xx) xxxxx-xxxx"
 										validation={{ required: true }}
 										mask="(99) 99999-9999"
-										pattern={/^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}-[0-9]{4}$/}
+										pattern={/(\(?\d{2}\)?\s)?(\d{4,5}-\d{4})/}
 									/>
 								</Cell>
 								<Cell col={2}>
