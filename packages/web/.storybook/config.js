7c34a7120eab81c20ca99580a1fe463b27aad11e
Davi de Oliveira <davioliveira.dev@gmail.com>
2020-08-28 18:20:20 -0300

    remove old config storybook

--- packages/web/.storybook/config.js
+++
@@ -1,6 +0,0 @@
-import { addDecorator } from '@storybook/react';
-import themeDecorator from './theme-decorator';
-import routerDecorator from './router-decorator';
-
-addDecorator(themeDecorator);
-addDecorator(routerDecorator);
