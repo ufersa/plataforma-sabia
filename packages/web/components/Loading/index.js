f0c59972e215e448b339b2c2e8ecd80d0b0ac109
mateus4k <mateus4k@protonmail.ch>
2020-08-28 22:39:04 -0300

    feat(loading): create variants

--- packages/web/components/Loading/index.js
+++ packages/web/components/Loading/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Loader } from './styles';
 
-const Loading = () => {
+const Loading = ({ variant }) => {
 	return (
 		<Container>
-			<Loader />
+			<Loader variant={variant} />
 		</Container>
 	);
+};
+
+Loading.propTypes = {
+	variant: PropTypes.oneOf(['primary', 'secondary', 'white']),
+};
+
+Loading.defaultProps = {
+	variant: 'primary',
 };
 
 export default Loading;
--- packages/web/components/Loading/styles.js
+++ packages/web/components/Loading/styles.js
@@ -1,14 +1,25 @@
 import styled from 'styled-components';
 import BeatLoader from 'react-spinners/BeatLoader';
 
+const getLoaderVariantColor = (variant, colors) => {
+	switch (variant) {
+		case 'primary':
+		default:
+			return colors.primary;
+		case 'secondary':
+			return colors.lightGray;
+		case 'white':
+			return colors.white;
+	}
+};
+
 export const Container = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	transition: 1s height;
 `;
 
-export const Loader = styled(BeatLoader).attrs(({ theme: { colors } }) => ({
+export const Loader = styled(BeatLoader).attrs(({ variant, theme: { colors } }) => ({
 	size: 35,
-	color: colors.primary,
+	color: getLoaderVariantColor(variant, colors),
 }))``;
