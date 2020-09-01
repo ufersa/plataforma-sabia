33025b94d287325e81baa7cd3c5161d4de22427f
NaylsonFerreira <naylsonfsa@gmail.com>
2020-09-01 00:29:16 -0300

    Translation

---
+++ packages/admin/src/components/LocaleSwitcher.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import { useLocale, useSetLocale, withTranslate } from 'react-admin';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+	ToggleButtonGroup: {
+		backgroundColor: 'white',
+	},
+});
+
+const LocaleSwitcher = () => {
+	const locale = useLocale();
+	const setLocale = useSetLocale();
+	const classes = useStyles();
+	const handleOnChange = (event, value) => setLocale(value || 'en');
+	return (
+		<div>
+			<ToggleButtonGroup
+				className={classes.ToggleButtonGroup}
+				size="small"
+				value={locale}
+				exclusive
+				onChange={handleOnChange}
+			>
+				<ToggleButton value="pt-br">PT</ToggleButton>
+				<ToggleButton value="en">EN</ToggleButton>
+			</ToggleButtonGroup>
+		</div>
+	);
+};
+
+export default withTranslate(LocaleSwitcher);
---
+++ packages/admin/src/components/Menu.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useSelector } from 'react-redux';
+import { useMediaQuery } from '@material-ui/core';
+import { MenuItemLink, getResources } from 'react-admin';
+import DefaultIcon from '@material-ui/icons/ViewList';
+
+const Menu = ({ onMenuClick, logout }) => {
+	const isXSmall = useMediaQuery((theme) => theme.breakpoints.down('xs'));
+	const open = useSelector((state) => state.admin.ui.sidebarOpen);
+	const resources = useSelector(getResources);
+	return (
+		<div>
+			{resources.map((resource) => (
+				<MenuItemLink
+					key={resource.name}
+					to={`/${resource.name}`}
+					primaryText={(resource.options && resource.options.label) || resource.name}
+					leftIcon={resource.icon ? <resource.icon /> : <DefaultIcon />}
+					onClick={onMenuClick}
+					sidebarIsOpen={open}
+				/>
+			))}
+			{isXSmall && logout}
+		</div>
+	);
+};
+Menu.propTypes = {
+	logout: PropTypes.shape({}).isRequired,
+	onMenuClick: PropTypes.func,
+};
+Menu.defaultProps = {
+	onMenuClick: () => {},
+};
+export default Menu;
