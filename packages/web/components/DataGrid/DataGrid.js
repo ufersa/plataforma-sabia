d8691b972db3927cf57f31c7b13c3b632cdca532
luizeboli <unifelicio@gmail.com>
2020-09-02 15:51:26 -0300

    feat(web): add pagination and ordering controls to DataGrid component

--- packages/web/components/DataGrid/DataGrid.js
+++ packages/web/components/DataGrid/DataGrid.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { Title } from '../Common';
 import { Link } from '../Link';
-import { Container, Grid, Row, Item, NoDataContainer } from './styles';
+import {
+	Container,
+	Grid,
+	Row,
+	Item,
+	NoDataContainer,
+	ArrowUpIcon,
+	ArrowDownIcon,
+	PaginationContainer,
+	ArrowLeftIcon,
+	ArrowRightIcon,
+	DoubleArrowLeftIcon,
+	DoubleArrowRightIcon,
+} from './styles';
 
-const DataGrid = ({ data, title, rowLink }) => {
+const DataGrid = ({
+	data,
+	title,
+	currentPage,
+	totalPages,
+	totalItems,
+	itemsPerPage,
+	currentOrder,
+	handlePagination,
+	handleOrder,
+	rowLink,
+	enablePagination,
+}) => {
+	const lastClickedHeader = useRef(null);
 	const headings = data.length > 0 ? Object.keys(data[0]) : [];
 	const { t } = useTranslation(['datagrid']);
+
+	const handleHeaderClick = (e) => {
+		const heading = e.currentTarget.dataset.name;
+		lastClickedHeader.current = heading;
+		handleOrder(heading);
+	};
+
+	const getItemsCountByPage = () => {
+		let firstItem;
+		let lastItem;
+
+		if (currentPage === 1) {
+			firstItem = 1;
+			lastItem = data.length;
+		} else if (currentPage === totalPages) {
+			firstItem = totalItems - data.length + 1;
+			lastItem = totalItems;
+		} else {
+			firstItem = (currentPage - 1) * itemsPerPage + 1;
+			lastItem = data.length + (currentPage - 1) * itemsPerPage;
+		}
+
+		return `${firstItem} - ${lastItem} ${t('paginationLabel')} ${totalItems}`;
+	};
 
 	return (
 		<Container>
@@ -18,10 +68,56 @@ const DataGrid = ({ data, title, rowLink }) => {
 			)}
 			{data.length > 0 ? (
 				<Grid>
+					{enablePagination && (
+						<PaginationContainer>
+							<button
+								type="button"
+								onClick={() => handlePagination(1)}
+								disabled={currentPage === 1}
+								aria-label="First Page"
+							>
+								<DoubleArrowLeftIcon />
+							</button>
+							<button
+								type="button"
+								onClick={() => handlePagination(currentPage - 1)}
+								disabled={currentPage === 1}
+								aria-label="Previous Page"
+							>
+								<ArrowLeftIcon />
+							</button>
+							<span>{getItemsCountByPage()}</span>
+							<button
+								type="button"
+								onClick={() => handlePagination(currentPage + 1)}
+								disabled={currentPage === totalPages}
+								aria-label="Next Page"
+							>
+								<ArrowRightIcon />
+							</button>
+							<button
+								type="button"
+								onClick={() => handlePagination(totalPages)}
+								disabled={currentPage === totalPages}
+								aria-label="Last Page"
+							>
+								<DoubleArrowRightIcon />
+							</button>
+						</PaginationContainer>
+					)}
 					<Row header columns={headings.length}>
 						{headings.map((heading) => (
-							<Item key={heading} data-name={heading}>
+							<Item
+								data-name={heading}
+								key={heading}
+								onClick={handleHeaderClick}
+								clickAble
+							>
 								{heading}
+								{lastClickedHeader.current === heading &&
+									currentOrder === 'ASC' && <ArrowUpIcon />}
+								{lastClickedHeader.current === heading &&
+									currentOrder === 'DESC' && <ArrowDownIcon />}
 							</Item>
 						))}
 					</Row>
@@ -55,12 +151,28 @@ DataGrid.propTypes = {
 			id: PropTypes.number,
 		}),
 	).isRequired,
+	currentPage: PropTypes.number,
+	totalPages: PropTypes.number,
+	totalItems: PropTypes.number,
+	itemsPerPage: PropTypes.number,
+	currentOrder: PropTypes.string,
+	handlePagination: PropTypes.func,
+	handleOrder: PropTypes.func,
 	rowLink: PropTypes.string,
+	enablePagination: PropTypes.bool,
 };
 
 DataGrid.defaultProps = {
 	title: null,
+	currentPage: 1,
+	totalPages: 1,
+	totalItems: 1,
+	itemsPerPage: 1,
+	currentOrder: '',
+	handlePagination: () => {},
+	handleOrder: () => {},
 	rowLink: '',
+	enablePagination: false,
 };
 
 export default DataGrid;
--- packages/web/components/DataGrid/styles.js
+++ packages/web/components/DataGrid/styles.js
@@ -1,4 +1,12 @@
 import styled, { css } from 'styled-components';
+import {
+	AiFillCaretDown,
+	AiFillCaretUp,
+	AiOutlineLeft,
+	AiOutlineRight,
+	AiOutlineDoubleLeft,
+	AiOutlineDoubleRight,
+} from 'react-icons/ai';
 
 export const Container = styled.section`
 	width: 100%;
@@ -57,23 +65,73 @@ export const Row = styled.li`
 `;
 
 export const Item = styled.div`
-	padding: 2rem 1rem;
+	${({ clickAble }) => css`
+		padding: 2rem 1rem;
 
-	@media screen and (max-width: ${({ theme }) => theme.screens.large}px) {
-		::before {
-			content: attr(data-name);
-			color: ${({ theme }) => theme.colors.secondary};
-			font-weight: bold;
+		@media screen and (max-width: ${({ theme }) => theme.screens.large}px) {
+			::before {
+				content: attr(data-name);
+				color: ${({ theme }) => theme.colors.secondary};
+				font-weight: bold;
+			}
+
+			display: grid;
+			grid-template-columns: minmax(9rem, 40%) 1fr;
+			text-align: left;
 		}
 
-		display: grid;
-		grid-template-columns: minmax(9rem, 40%) 1fr;
-		text-align: left;
-	}
+		${clickAble &&
+			css`
+				cursor: pointer;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			`}
+	`}
 `;
 
 export const NoDataContainer = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+`;
+
+const iconCss = css`
+	margin-left: 0.5rem;
+`;
+
+export const ArrowDownIcon = styled(AiFillCaretDown)`
+	${iconCss}
+`;
+export const ArrowUpIcon = styled(AiFillCaretUp)`
+	${iconCss}
+`;
+
+export const ArrowLeftIcon = styled(AiOutlineLeft)`
+	margin-right: 0.5rem;
+`;
+
+export const ArrowRightIcon = styled(AiOutlineRight)`
+	${iconCss}
+`;
+
+export const DoubleArrowLeftIcon = styled(AiOutlineDoubleLeft)``;
+
+export const DoubleArrowRightIcon = styled(AiOutlineDoubleRight)``;
+
+export const PaginationContainer = styled.div`
+	margin-bottom: 1.2rem;
+
+	> button {
+		background: none;
+		border: none;
+
+		&:disabled {
+			cursor: not-allowed;
+		}
+	}
+
+	> span {
+		font-size: 1.4rem;
+	}
 `;
