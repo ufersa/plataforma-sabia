cf79f6dbb81b792f382fcc708d3c40cb384f2f46
mateus4k <mateus4k@protonmail.ch>
2020-09-06 16:41:12 -0300

    style(loading): improve formatting

--- packages/web/components/Loading/Spinner.js
+++ packages/web/components/Loading/Spinner.js
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types';
 
 import { Container, Loader } from './styles';
 
-const Spinner = ({ variant }) => {
-	return (
-		<Container>
-			<Loader variant={variant} />
-		</Container>
-	);
-};
+const Spinner = ({ variant }) => (
+	<Container>
+		<Loader variant={variant} />
+	</Container>
+);
 
 Spinner.propTypes = {
 	variant: PropTypes.oneOf(['primary', 'secondary', 'white']),
