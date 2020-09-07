8fe565229db64f6120ae008d764996986aff1960
Lorena Calaça <lorena.calaca.od@gmail.com>
2020-09-07 19:05:55 -0300

    fix(test): setup icu at setupTests

--- packages/web/setupTests.js
+++ packages/web/setupTests.js
@@ -3,5 +3,6 @@ import '@testing-library/jest-dom/extend-expect';
 import 'jest-styled-components';
 import 'core-js';
 import '@testing-library/jest-dom';
+import 'full-icu';
 
 window.MutationObserver = MutationObserver;
