a948e5b7b371a6003ba15225a20d8d09c901a35d
Naylson Ferreira <naylsonfsa@gmail.com>
2020-09-10 09:58:43 -0300

    Update packages/api/app/Validators/DeleteMany.js
    Co-authored-by: Alexandre Ádames <alexandre.adames@gmail.com>

--- packages/api/app/Validators/DeleteMany.js
+++ packages/api/app/Validators/DeleteMany.js
@@ -2,8 +2,11 @@ const BaseValidator = use('App/Validators/BaseValidator');
 
 class DeleteMany extends BaseValidator {
 	get rules() {
+		const get_resource = this.ctx.request.url().split('/')[1];
+		const resource = get_resource;
 		return {
 			ids: 'required',
+			'ids.*': `exists:${resource},id`,
 		};
 	}
 
