7348962d2c3ca4c1902b87ad599b8ad401070e5d
NaylsonFerreira <naylsonfsa@gmail.com>
2020-09-24 20:48:09 -0300

    improve validator

--- packages/api/app/Validators/Upload.js
+++ packages/api/app/Validators/Upload.js
@@ -3,7 +3,8 @@ const BaseValidator = use('App/Validators/BaseValidator');
 class Upload extends BaseValidator {
 	get rules() {
 		return {
-			files: 'required',
+			files:
+				'required|file|file_ext:jpg,jpeg,jfif,pjpeg,pjp,png,webp,pdf|file_size:2mb|file_types:image,application',
 			'files.*': 'file',
 		};
 	}
