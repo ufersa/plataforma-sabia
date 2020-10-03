42dfbe522ff2906b187c414941b82086ee72d022
luizeboli <unifelicio@gmail.com>
2020-10-03 20:59:08 -0300

    fix(web/enums): typo

--- packages/web/utils/enums/technology.enums.js
+++ packages/web/utils/enums/technology.enums.js
@@ -2,7 +2,7 @@ export const STATUS = {
 	DRAFT: 'draft',
 	PENDING: 'pending',
 	IN_REVIEW: 'in_review',
-	REQUESTED_CHANGED: 'requested_changes',
+	REQUESTED_CHANGES: 'requested_changes',
 	CHANGES_MADE: 'changes_made',
 	APPROVED: 'approved',
 	REJECTED: 'rejected',
