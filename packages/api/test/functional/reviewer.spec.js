63ba0d0eddd9e9ba6f1bc2748fd872353febfcd5
Alexandre <alexandre.adames@gmail.com>
2020-10-03 16:43:34 -0300

    Revision Tests

--- packages/api/test/functional/reviewer.spec.js
+++ packages/api/test/functional/reviewer.spec.js
@@ -3,12 +3,14 @@ const { test, trait } = use('Test/Suite')('Reviewer');
 const User = use('App/Models/User');
 const Reviewer = use('App/Models/Reviewer');
 const Taxonomy = use('App/Models/Taxonomy');
+const Technology = use('App/Models/Technology');
+const Revision = use('App/Models/Revision');
 
 trait('Test/ApiClient');
 trait('Auth/Client');
 trait('DatabaseTransactions');
 
-const { roles } = require('../../app/Utils');
+const { antl, errors, errorPayload, roles } = require('../../app/Utils');
 
 const user = {
 	email: 'sabiatestingemail@gmail.com',
@@ -31,6 +33,32 @@ const reviewer = {
 	first_name: 'FirstName',
 	last_name: 'LastName',
 	role: roles.REVIEWER,
+};
+
+const researcher = {
+	email: 'researcher@gmail.com',
+	password: '123123',
+	first_name: 'FirstName',
+	last_name: 'LastName',
+	role: roles.RESEARCHER,
+};
+
+const technology = {
+	title: 'Test Title',
+	description: 'Test description',
+	private: 0,
+	patent: 1,
+	patent_number: '0001/2020',
+	primary_purpose: 'Test primary purpose',
+	secondary_purpose: 'Test secondary purpose',
+	application_mode: 'Test application mode',
+	application_examples: 'Test application example',
+	installation_time: 2000,
+	solves_problem: 'Solves problem test',
+	entailes_problem: 'Entailes problem test',
+	requirements: 'Requirements test',
+	risks: 'Test risks',
+	contribution: 'Test contribution',
 };
 
 test('GET /reviewers get list of reviewers', async ({ client, assert }) => {
@@ -119,4 +147,93 @@ test('PUT /reviewers/:id/update-status udpates reviewer status.', async ({ clien
 
 	response.assertStatus(200);
 	response.assertJSONSubset(approvedReviewer.toJSON());
+});
+
+test('POST revisions/:technology reviewer trying to review a technology no attributed for him.', async ({
+	client,
+}) => {
+	const reviewerUser = await User.create(reviewer);
+	const approvedReviewer = await Reviewer.create({ status: 'approved' });
+	await approvedReviewer.user().associate(reviewerUser);
+
+	const pendingTechnology = await Technology.create(technology);
+
+	const response = await client
+		.post(`/revisions/${pendingTechnology.id}`)
+		.loginVia(reviewerUser, 'jwt')
+		.send({
+			description: 'Your technology was rejected',
+			assessment: 'rejected',
+		})
+		.end();
+
+	response.assertStatus(403);
+	response.assertJSONSubset(
+		errorPayload(errors.UNAUTHORIZED_ACCESS, antl('error.permission.unauthorizedAccess')),
+	);
+});
+
+test('POST revisions/:technology reviewer trying to review a technology with no allowed status.', async ({
+	client,
+}) => {
+	const reviewerUser = await User.create(reviewer);
+	const approvedReviewer = await Reviewer.create({ status: 'approved' });
+	await approvedReviewer.user().associate(reviewerUser);
+
+	const pendingTechnology = await Technology.create(technology);
+	pendingTechnology.status = 'pending';
+	await pendingTechnology.save();
+	await approvedReviewer.technologies().attach([pendingTechnology.id]);
+
+	const response = await client
+		.post(`/revisions/${pendingTechnology.id}`)
+		.loginVia(reviewerUser, 'jwt')
+		.send({
+			description: 'Your technology was rejected',
+			assessment: 'rejected',
+		})
+		.end();
+
+	response.assertStatus(400);
+	response.assertJSONSubset(
+		errorPayload(
+			errors.STATUS_NO_ALLOWED_FOR_REVIEW,
+			antl('error.reviewer.statusNoAllowedForReview', {
+				status: pendingTechnology.status,
+			}),
+		),
+	);
+});
+
+test('POST revisions/:technology reviewer makes a revision.', async ({ client, assert }) => {
+	const reviewerUser = await User.create(reviewer);
+	const researcherUser = await User.create(researcher);
+	const approvedReviewer = await Reviewer.create({ status: 'approved' });
+	await approvedReviewer.user().associate(reviewerUser);
+
+	const pendingTechnology = await Technology.create(technology);
+	await pendingTechnology.users().attach([researcherUser.id]);
+	pendingTechnology.status = 'in_review';
+	await pendingTechnology.save();
+	await approvedReviewer.technologies().attach([pendingTechnology.id]);
+
+	const response = await client
+		.post(`/revisions/${pendingTechnology.id}`)
+		.loginVia(reviewerUser, 'jwt')
+		.send({
+			description: 'Your technology was approved',
+			assessment: 'approved',
+		})
+		.end();
+
+	const revision = await Revision.findOrFail(response.body.id);
+	await revision.loadMany(['technology', 'reviewer.user']);
+
+	const reviewedTechnology = await Technology.findOrFail(pendingTechnology.id);
+
+	assert.equal(revision.assessment, reviewedTechnology.status);
+
+	response.assertStatus(200);
+	response.body.attachment_id = null;
+	response.assertJSONSubset(revision.toJSON());
 });
