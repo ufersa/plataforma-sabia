59d7d3376c81914398253a8b33f199d6539ea657
luizeboli <unifelicio@gmail.com>
2020-10-03 21:15:15 -0300

    refactor(web): change getPages to receive user object and filter pages based on role

--- packages/web/components/UserProfile/pages.js
+++ packages/web/components/UserProfile/pages.js
@@ -5,37 +5,45 @@ import { FiSearch } from 'react-icons/fi';
  * Translates profile sections and pages.
  *
  * @param {Function} t The function to translate the pages and sections titles.
+ * @param {object} user The authenticated user object to filter pages
  * @returns {Array} The translated sections and pages.
  */
-const getPages = (t) => [
-	{
-		id: 1,
-		title: t('profile:userArea'),
-		pages: [
-			{ title: t('profile:myProfile'), href: '', icon: FaRegUserCircle },
-			{ title: t('profile:messages'), href: '', icon: FaEnvelope },
-			{ title: t('profile:favoriteTechnologies'), href: '/bookmarks', icon: FaHeart },
-		],
-	},
-	{
-		id: 2,
-		title: t('profile:researcherArea'),
-		pages: [
-			{ title: t('profile:myTechnologies'), href: '/technologies', icon: FaRegListAlt },
-			{ title: t('profile:myNegotiations'), href: '', icon: FaSuitcase },
-		],
-	},
-	{
-		id: 3,
-		title: t('profile:reviewerArea'),
-		pages: [
-			{
-				title: t('profile:curateTechnologies'),
-				href: '/curate-technologies',
-				icon: FiSearch,
-			},
-		],
-	},
-];
+const getPages = (t, user) => {
+	const pages = [
+		{
+			id: 1,
+			title: t('profile:userArea'),
+			pages: [
+				{ title: t('profile:myProfile'), href: '', icon: FaRegUserCircle },
+				{ title: t('profile:messages'), href: '', icon: FaEnvelope },
+				{ title: t('profile:favoriteTechnologies'), href: '/bookmarks', icon: FaHeart },
+			],
+		},
+		{
+			id: 2,
+			title: t('profile:researcherArea'),
+			pages: [
+				{ title: t('profile:myTechnologies'), href: '/technologies', icon: FaRegListAlt },
+				{ title: t('profile:myNegotiations'), href: '', icon: FaSuitcase },
+			],
+		},
+	];
+
+	if (user.role.role === 'REVIEWER') {
+		pages.push({
+			id: 3,
+			title: t('profile:reviewerArea'),
+			pages: [
+				{
+					title: t('profile:curateTechnologies'),
+					href: '/curate-technologies',
+					icon: FiSearch,
+				},
+			],
+		});
+	}
+
+	return pages;
+};
 
 export default getPages;
