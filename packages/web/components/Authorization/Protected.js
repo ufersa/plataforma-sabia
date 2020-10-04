1a8be1d68f5926395357d63b90e9bafeb30221a6
luizeboli <unifelicio@gmail.com>
2020-10-03 21:19:33 -0300

    fix(web): change protected component to compare role from user role object

--- packages/web/components/Authorization/Protected.js
+++ packages/web/components/Authorization/Protected.js
@@ -21,7 +21,7 @@ const Protected = ({
 	const router = useRouter();
 
 	const isLoggedIn = !!user?.email;
-	const isAuthorized = isLoggedIn && (role ? role === user.role : true);
+	const isAuthorized = isLoggedIn && (role ? role === user.role.role : true);
 
 	useEffect(() => {
 		if (onlyUnauthorizedMessage) {
