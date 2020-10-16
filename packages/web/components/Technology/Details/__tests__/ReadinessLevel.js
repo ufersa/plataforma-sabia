3521b5990bcc74d8f14f12375b95a5b36046a519
Luiz Felicio <unifelicio@gmail.com>
2020-10-16 10:52:59 -0300

    Detalhamento de Tecnologia - Estágios de Desenvolvimento (#496)
    * refactor(web): move normalizeTrl to technology helpers
    
    * refactor(web): set ReadinessLevel component as responsible for getting stage slug
    
    * feat(web): add TRL to tech details about section
    
    * test(web): update snapshot
    
    * refactor(web/trl): change ref method to calculate image height when ref changes
    
    * refactor(web/trl): apply suggestion from @mateus4k
    
    * refactor(web/trl): apply suggestion from @mateus4k

--- packages/web/components/Technology/Details/__tests__/ReadinessLevel.js
+++ packages/web/components/Technology/Details/__tests__/ReadinessLevel.js
@@ -5,12 +5,17 @@ import { TechnologyProvider } from '../../TechnologyProvider';
 import ReadinessLevel from '../ReadinessLevel';
 
 const technologyMock = {
-	taxonomies: {
-		readinessLevel: {
+	terms: [
+		{
+			term:
+				'Nível 4 - Verificação funcional de componente e/ou subsistema em ambiente laboratorial',
 			slug:
-				'nivel-4-verificacao-funcional-de-componente-e-ou-subsistema-em-ambiente-laboratorial',
+				'nivel-4-verificacao-funcional-de-componente-e/ou-subsistema-em-ambiente-laboratorial',
+			taxonomy: {
+				taxonomy: 'STAGE',
+			},
 		},
-	},
+	],
 };
 
 describe('<ReadinessLevel />', () => {
