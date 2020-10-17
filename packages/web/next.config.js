0d15340ec6de2dcf3658b3c39d31b7608623db93
marcusmota <marcushcsm@gmail.com>
2020-10-17 14:58:33 +0100

    algolia adjustments (#505)

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -4,6 +4,7 @@ module.exports = {
 	env: {
 		ALGOLIA_SEARCH_KEY: process.env.ALGOLIA_SEARCH_KEY,
 		ALGOLIA_APPLICATION_ID: process.env.ALGOLIA_APPLICATION_ID,
+		ALGOLIA_INDEX_SUFIX: process.env.ALGOLIA_INDEX_SUFIX,
 		API_URL: process.env.API_URL,
 		GOOGLE_MAPS_KEY: process.env.GOOGLE_MAPS_KEY,
 	},
