dfd82daa924dd80ebc47d3de2c2adff0dca0e199
Davi de Oliveira <davioliveira.dev@gmail.com>
2020-10-17 15:09:57 -0300

    Correção na imagem inicial (#503)
    * Add random images to hero and remove hero.jpg
    
    * Remove or 0 to search index
    
    * Add + 1 to length array
    
    * Add prop 'image' into hero component
    
    * remove unused var

--- packages/web/components/Hero/Hero.js
+++ packages/web/components/Hero/Hero.js
@@ -6,8 +6,14 @@ import { HeroSearch } from './HeroSearch';
 const Hero = () => {
 	const { t } = useTranslation(['common']);
 
+	const heroImage = () => {
+		const heroImgs = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg'];
+		const heroIndexImg = Math.floor(Math.random() * (heroImgs.length + 1));
+		return heroImgs[heroIndexImg];
+	};
+
 	return (
-		<HeroImage>
+		<HeroImage image={heroImage}>
 			<Content>
 				<h1>{t('common:heroTitle')}</h1>
 				<p>{t('common:heroSubTitle')}</p>
