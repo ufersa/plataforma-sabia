4cf7fdab9bc3b186cfeaf54905f85e01e97f82d8
Mateus <mateus4k@protonmail.ch>
2020-10-19 21:20:25 -0300

    feat: remove uncontrolled content input for ux proposes

--- packages/web/components/Modal/AddReviewModal/AddReview/index.js
+++ packages/web/components/Modal/AddReviewModal/AddReview/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { cache } from 'swr';
 import { createTechnologyReview } from '../../../../services';
@@ -16,24 +16,26 @@ import {
 } from './styles';
 
 const AddReview = ({ technology }) => {
-	const contentRef = useRef(null);
 	const { closeModal } = useModal();
 	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [isAbleToSubmit, setIsAbleToSubmit] = useState(false);
+	const [content, setContent] = useState(null);
 	const [positivePoints, setPositivePoints] = useState([]);
 	const [negativePoints, setNegativePoints] = useState([]);
 	const [rating, setRating] = useState(0);
 
-	// eslint-disable-next-line consistent-return
+	useEffect(() => {
+		if (!content?.length || !rating || !positivePoints.length || !negativePoints.length) {
+			setIsAbleToSubmit(false);
+			return;
+		}
+
+		setIsAbleToSubmit(true);
+	}, [setIsAbleToSubmit, negativePoints.length, positivePoints.length, rating, content]);
+
 	const handleSubmit = async (event) => {
 		event.preventDefault();
-		setIsSubmitting(true);
-
-		const { value: content } = contentRef.current;
-
-		if (!content || !rating || !positivePoints.length || !negativePoints.length) {
-			setIsSubmitting(false);
-			return toast.error('Todos os campos são obrigatórios.');
-		}
+		setIsSubmitting(false);
 
 		const result = await createTechnologyReview({
 			technologyId: technology.id,
@@ -58,7 +60,7 @@ const AddReview = ({ technology }) => {
 		<Container>
 			<form onSubmit={handleSubmit}>
 				<p>Como foi sua experiência com essa tecnologia?</p>
-				<TextArea name="content" ref={contentRef} />
+				<TextArea onChange={(e) => setContent(e.currentTarget.value)} name="content" />
 
 				<Points label="Quais pontos positivos?" onPointsUpdate={setPositivePoints} />
 				<Points label="Quais pontos negativos?" onPointsUpdate={setNegativePoints} />
@@ -70,8 +72,8 @@ const AddReview = ({ technology }) => {
 
 				<ButtonsContainer>
 					<CloseButton onClick={closeModal}>Cancelar</CloseButton>
-					<SubmitButton disabled={isSubmitting}>
-						{!isSubmitting ? 'Registrar Avaliação' : 'Cadastrando...'}
+					<SubmitButton disabled={!isAbleToSubmit || isSubmitting}>
+						<span>{!isSubmitting ? 'Registrar Avaliação' : 'Cadastrando...'}</span>
 					</SubmitButton>
 				</ButtonsContainer>
 			</form>
