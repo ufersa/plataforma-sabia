f684fb8e91e2d991e1e7c0372a1c0a2d6123c591
Naylson Ferreira <naylsonfsa@gmail.com>
2020-10-21 22:33:41 -0300

    Refactored seeds (#492)
    * refactored seeds
    
    * updated TechnologySeeder
    
    * Update packages/api/database/seeds/4_TechnologySeeder.js
    
    Co-authored-by: Alexandre Ádames <alexandre.adames@gmail.com>
    
    * Updated Comments
    
    Co-authored-by: Naylson  Ferreira <naylsonferreira@MacBook-Pro-de-Naylson.local>
    Co-authored-by: Alexandre Ádames <alexandre.adames@gmail.com>

--- packages/api/database/seeds/8_TechnologyCostSeeder.js
+++ packages/api/database/seeds/8_TechnologyCostSeeder.js
@@ -16,12 +16,14 @@ const Technology = use('App/Models/Technology');
 class TechnologyCostSeeder {
 	async run() {
 		const technologies = await Technology.all();
-		for (const technology of technologies.rows) {
-			const technologyCost = await Factory.model('App/Models/TechnologyCost').create();
-			await technologyCost.technology().associate(technology);
-			const costs = await Factory.model('App/Models/Cost').createMany(3);
-			await technologyCost.costs().saveMany(costs);
-		}
+		await Promise.all(
+			technologies.rows.map(async (technology) => {
+				const technologyCost = await Factory.model('App/Models/TechnologyCost').create();
+				await technologyCost.technology().associate(technology);
+				const costs = await Factory.model('App/Models/Cost').createMany(3);
+				await technologyCost.costs().saveMany(costs);
+			}),
+		);
 	}
 }
 
