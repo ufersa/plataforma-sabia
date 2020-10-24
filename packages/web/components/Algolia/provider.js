bb155b766dceb1af19366310283aa81af81fef3c
Marcus Mota <marcushcsm@gmail.com>
2020-10-25 00:18:45 +0100

    refactor the algolia index

--- packages/web/components/Algolia/provider.js
+++ packages/web/components/Algolia/provider.js
@@ -6,7 +6,7 @@ import config from '../../config';
 
 const algoliaClient = algoliasearch(config.ALGOLIA_APPLICATION_ID, config.ALGOLIA_SEARCH_KEY);
 
-const defaultIndexName = `searchable_data_${config.ALGOLIA_INDEX_SUFIX}`;
+const defaultIndexName = `${config.ALGOLIA_INDEX}`;
 
 export const algoliaDefaultConfig = {
 	searchClient: algoliaClient,
--- packages/web/config.js
+++ packages/web/config.js
@@ -5,7 +5,7 @@ if (typeof window === 'undefined' || process.env.APP_ENV === 'test') {
 	const production = {
 		ALGOLIA_SEARCH_KEY: '8ea4ffa0588206bb0c0751294097e875',
 		ALGOLIA_APPLICATION_ID: 'GC7K0ETHXB',
-		ALGOLIA_INDEX_SUFIX: 'production',
+		ALGOLIA_INDEX: 'searchable_data_production',
 		API_URL: 'http://api.plataformasabia.com',
 		GOOGLE_MAPS_KEY: 'AIzaSyDlQrq14K2OTjUxioB4fW7NJTzZQ2ZFtxA',
 		APP_ENV: 'production',
@@ -14,7 +14,7 @@ if (typeof window === 'undefined' || process.env.APP_ENV === 'test') {
 	const staging = {
 		ALGOLIA_SEARCH_KEY: '8ea4ffa0588206bb0c0751294097e875',
 		ALGOLIA_APPLICATION_ID: 'GC7K0ETHXB',
-		ALGOLIA_INDEX_SUFIX: 'staging',
+		ALGOLIA_INDEX: 'searchable_data_staging',
 		API_URL: 'http://api-staging.plataformasabia.com',
 		GOOGLE_MAPS_KEY: 'AIzaSyDlQrq14K2OTjUxioB4fW7NJTzZQ2ZFtxA',
 		APP_ENV: 'staging',
@@ -23,7 +23,7 @@ if (typeof window === 'undefined' || process.env.APP_ENV === 'test') {
 	const test = {
 		ALGOLIA_SEARCH_KEY: '8ea4ffa0588_test',
 		ALGOLIA_APPLICATION_ID: 'GC7K0E_test',
-		ALGOLIA_INDEX_SUFIX: 'test',
+		ALGOLIA_INDEX: 'searchable_data_test',
 		API_URL: 'http://api-test.plataformasabia.com',
 		GOOGLE_MAPS_KEY: 'AIzaSyDlQrq14K2OTj_test',
 		APP_ENV: 'test',
@@ -32,7 +32,7 @@ if (typeof window === 'undefined' || process.env.APP_ENV === 'test') {
 	const development = {
 		ALGOLIA_SEARCH_KEY: process.env.ALGOLIA_SEARCH_KEY,
 		ALGOLIA_APPLICATION_ID: process.env.ALGOLIA_APPLICATION_ID,
-		ALGOLIA_INDEX_SUFIX: process.env.ALGOLIA_INDEX_SUFIX,
+		ALGOLIA_INDEX: process.env.ALGOLIA_INDEX,
 		API_URL: process.env.API_URL,
 		GOOGLE_MAPS_KEY: process.env.GOOGLE_MAPS_KEY,
 		APP_ENV: 'development',
@@ -44,7 +44,6 @@ if (typeof window === 'undefined' || process.env.APP_ENV === 'test') {
 		...(process.env.APP_ENV === 'staging' ? staging : {}),
 		...(process.env.APP_ENV === 'production' ? production : {}),
 	};
-	console.log(config);
 } else {
 	config = window.env || {};
 }
