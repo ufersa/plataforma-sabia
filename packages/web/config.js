59abe0732cb174bfb597674bf7bbd4d8ab9b5865
Marcus Mota <marcushcsm@gmail.com>
2020-10-24 15:11:35 +0100

    fix development api url

--- packages/web/config.js
+++ packages/web/config.js
@@ -24,7 +24,7 @@ if (typeof window === 'undefined') {
 		ALGOLIA_SEARCH_KEY: process.env.ALGOLIA_SEARCH_KEY,
 		ALGOLIA_APPLICATION_ID: process.env.ALGOLIA_APPLICATION_ID,
 		ALGOLIA_INDEX_SUFIX: process.env.ALGOLIA_INDEX_SUFIX,
-		API_URL: 'http://api-staging.plataformasabia.com',
+		API_URL: process.env.API_URL,
 		GOOGLE_MAPS_KEY: process.env.GOOGLE_MAPS_KEY,
 		APP_ENV: 'development',
 	};
