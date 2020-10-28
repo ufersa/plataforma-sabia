e7e97cbe221fa1e58e7d58fb00ff9b499cd6273d
luizeboli <unifelicio@gmail.com>
2020-10-28 14:36:12 -0300

    feat(web/form): add variant to textfield

--- packages/web/components/Form/TextField.js
+++ packages/web/components/Form/TextField.js
@@ -1,26 +1,40 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import { InputFieldWrapper, InputLabel, InputError, Row } from './styles';
 import { validationErrorMessage } from '../../utils/helper';
 import Help from './Help';
 import RequiredIndicator from './Required/Indicator';
 
+const textareaModifiers = {
+	default: ({ colors, metrics }) => css`
+		background: white;
+		border: 1px solid ${colors.mediumGray};
+		border-radius: ${metrics.baseRadius}rem;
+		color: ${colors.lightGray};
+	`,
+	gray: ({ colors, metrics }) => css`
+		background: ${colors.lightGray4};
+		border: 1px solid ${colors.lightGray4};
+		border-radius: ${metrics.baseRadius}rem;
+	`,
+};
+
 export const StyledTextArea = styled.textarea`
-	width: 100%;
-	height: 12rem;
-	font-size: 1.4rem;
-	margin: 0.5rem 0;
-	padding: 1rem;
-	background: white;
-	border: 1px solid ${({ theme }) => theme.colors.mediumGray};
-	border-radius: 0.5rem;
-	color: ${({ theme }) => theme.colors.lightGray};
+	${({ theme: { colors, metrics }, variant }) => css`
+		width: 100%;
+		height: 12rem;
+		font-size: 1.4rem;
+		margin: 0.5rem 0;
+		padding: 1rem;
+
+		${!!variant && textareaModifiers[variant]({ colors, metrics })}
+	`}
 `;
 
-const TextField = ({ name, label, form, help, validation, wrapperCss, ...inputProps }) => {
+const TextField = ({ name, label, form, help, validation, wrapperCss, variant, ...inputProps }) => {
 	const { t } = useTranslation(['error']);
 	const { register, errors } = form;
 
@@ -38,6 +52,7 @@ const TextField = ({ name, label, form, help, validation, wrapperCss, ...inputPr
 					aria-label={label}
 					aria-required={validation.required}
 					ref={register(validation)}
+					variant={variant}
 					{...inputProps}
 				/>
 				{help && <Help id={name} label={label} HelpComponent={help} />}
@@ -62,6 +77,7 @@ TextField.propTypes = {
 		required: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
 	}),
 	wrapperCss: PropTypes.arrayOf(PropTypes.string),
+	variant: PropTypes.oneOf('default', 'gray'),
 };
 
 TextField.defaultProps = {
@@ -69,6 +85,7 @@ TextField.defaultProps = {
 	validation: {},
 	help: null,
 	wrapperCss: [],
+	variant: 'default',
 };
 
 export default TextField;
