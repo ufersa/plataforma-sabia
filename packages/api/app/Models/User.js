b76299c3c6336d6763969f204e989d1682c657c3
Mateus <mateus4k@protonmail.ch>
2020-10-28 23:34:04 -0300

    feat(institution): make relationship from user model

--- packages/api/app/Models/User.js
+++ packages/api/app/Models/User.js
@@ -150,6 +150,10 @@ class User extends Model {
 		return this.hasMany('App/Models/TechnologyComment');
 	}
 
+	institution() {
+		return this.belongsTo('App/Models/Institution');
+	}
+
 	generateToken(type) {
 		return this.tokens().create({
 			type,
