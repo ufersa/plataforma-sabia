7ce19eeba23b365f550a87bcbb978a5dba5270ae
Davi de Oliveira <davioliveira.dev@gmail.com>
2020-10-29 07:22:06 -0300

    Fix lint

--- packages/api/database/migrations/1603847242248_technology_videos_schema.js
+++ packages/api/database/migrations/1603847242248_technology_videos_schema.js
@@ -1,14 +1,12 @@
-'use strict'
-
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 
 class TechnologyVideosSchema extends Schema {
-  up () {
-    this.table('technologies', (table) => {
-      table.json('videos')
-    })
-  }
+	up () {
+		this.table('technologies', (table) => {
+			table.json('videos');
+		});
+	}
 }
 
-module.exports = TechnologyVideosSchema
+module.exports = TechnologyVideosSchema;
