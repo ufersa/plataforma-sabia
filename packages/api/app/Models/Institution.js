958c01a5386cbf88793621df6d9ffd0ba46f546b
Mateus <mateus4k@protonmail.ch>
2020-10-28 23:56:37 -0300

    feat(institution): add model withFilter scope

--- packages/api/app/Models/Institution.js
+++ packages/api/app/Models/Institution.js
@@ -2,6 +2,30 @@
 const Model = use('Model');
 
 class Institution extends Model {
+	/**
+	 * Runs the institution query with the provided filters.
+	 *
+	 * @param {object} query The query object.
+	 * @param {object} request The request object.
+	 * @returns {object}
+	 */
+	static async scopeWithFilters(query, request) {
+		const filters = request.all();
+
+		/**
+		 * Input: Rio de Janeiro
+		 * Output: %rio%de%janeiro%
+		 */
+		if (filters.city) {
+			const city = filters.city
+				.toLowerCase()
+				.split(' ')
+				.map((word, index) => (index ? `${word}%` : `%${word}%`))
+				.join('');
+			query.whereRaw('LOWER(city) LIKE :city', { city });
+		}
+	}
+
 	users() {
 		return this.hasMany('App/Models/User');
 	}
