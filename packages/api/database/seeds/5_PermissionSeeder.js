9073e37d504de5c864e887c76663a4f27a136fbf
Mateus <mateus4k@protonmail.ch>
2020-10-31 14:31:22 -0300

    add new permissions to seeds

--- packages/api/database/seeds/5_PermissionSeeder.js
+++ packages/api/database/seeds/5_PermissionSeeder.js
@@ -109,6 +109,16 @@ class PermissionSeeder {
 		]);
 		const uploadPermissions = await Permission.createMany([permissions.DELETE_UPLOAD]);
 
+		/** INSTITUTION MANAGEMENT */
+		const institutionsPermissions = await Permission.createMany([
+			permissions.UPDATE_INSTITUTION,
+			permissions.DELETE_INSTITUTION,
+		]);
+		const institutionPermissions = await Permission.createMany([
+			permissions.UPDATE_INSTITUTIONS,
+			permissions.DELETE_INSTITUTIONS,
+		]);
+
 		/** ADMIN ROLE */
 		/** The ADMIN user has all permissions */
 		const adminPermissionsIds = [
@@ -122,6 +132,7 @@ class PermissionSeeder {
 			...bookmarksPermissions,
 			...uploadsPermissions,
 			...technologyRevisionPermissions,
+			...institutionsPermissions,
 		].map((permission) => permission.id);
 		const adminRole = await Role.getRole(roles.ADMIN);
 		await adminRole.permissions().attach(adminPermissionsIds);
@@ -134,6 +145,7 @@ class PermissionSeeder {
 			...technologyReviewPermissions,
 			...bookmarkPermissions,
 			...uploadPermissions,
+			...institutionPermissions,
 		].map((permission) => permission.id);
 
 		const researcherRole = await Role.getRole(roles.RESEARCHER);
@@ -144,6 +156,7 @@ class PermissionSeeder {
 				technologyReviewsPermissions[0].id,
 				uploadsPermissions[0].id,
 				...researcherPermissions,
+				...institutionPermissions,
 			]);
 
 		/** REVIEWER ROLE */
@@ -160,6 +173,7 @@ class PermissionSeeder {
 				uploadsPermissions[0].id,
 				...researcherPermissions,
 				...reviewerPermissions,
+				...institutionPermissions,
 			]);
 
 		/** DEFAULT_USER ROLE */
@@ -171,6 +185,7 @@ class PermissionSeeder {
 				technologyReviewsPermissions[0].id,
 				uploadsPermissions[0].id,
 				...researcherPermissions,
+				...institutionPermissions,
 			]);
 	}
 }
