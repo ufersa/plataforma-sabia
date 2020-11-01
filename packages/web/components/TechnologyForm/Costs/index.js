188577919974a45d9e777b14d0ecacbf8bc4039d
luizeboli <unifelicio@gmail.com>
2020-11-01 20:40:58 -0300

    refactor(web/form): pass fields down to other tables

--- packages/web/components/TechnologyForm/Costs/index.js
+++ packages/web/components/TechnologyForm/Costs/index.js
@@ -86,12 +86,13 @@ const Costs = ({ form, data }) => {
 						/>
 					)}
 					// eslint-disable-next-line no-shadow
-					endComponent={({ append, emptyValue }) => (
+					endComponent={({ append, emptyValue, fields }) => (
 						<CostsTableFooter
 							collection="technologyCosts.costs.development_costs"
 							emptyValue={emptyValue}
 							append={append}
 							form={form}
+							fields={fields}
 						/>
 					)}
 				/>
@@ -154,12 +155,13 @@ const Costs = ({ form, data }) => {
 					/>
 				)}
 				// eslint-disable-next-line no-shadow
-				endComponent={({ append, emptyValue }) => (
+				endComponent={({ append, emptyValue, fields }) => (
 					<CostsTableFooter
 						collection="technologyCosts.costs.maintenance_costs"
 						emptyValue={emptyValue}
 						append={append}
 						form={form}
+						fields={fields}
 					/>
 				)}
 			/>
