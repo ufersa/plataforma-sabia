03012c69fe1db51659111d0f4e463579a1650272
luizeboli <unifelicio@gmail.com>
2020-11-04 11:21:41 -0300

    refactor(web): change search item to match new query suggestions

--- packages/web/components/Hero/HeroSearch/styles.js
+++ packages/web/components/Hero/HeroSearch/styles.js
@@ -1,47 +1,14 @@
-import styled from 'styled-components';
-import { Hits, Stats } from 'react-instantsearch-dom';
-
-export const StyledHits = styled(Hits)`
-	position: relative;
-
-	.ais-Hits-list {
-		z-index: 100;
-		position: absolute;
-		background-color: ${({ theme }) => theme.colors.white};
-		top: 0;
-		left: 0;
-		width: 100%;
-	}
-
-	.ais-Hits-item {
-		padding: 1rem;
-		border-bottom: 2px solid ${({ theme }) => theme.colors.border};
-	}
-
-	.ais-Highlight-highlighted {
-		background-color: ${({ theme }) => theme.colors.primary};
-	}
-`;
+import styled, { css } from 'styled-components';
+import { Stats } from 'react-instantsearch-dom';
 
 export const SearchItemContainer = styled.div`
 	width: 100%;
 	display: flex;
 	align-items: center;
-	min-height: 9rem;
-`;
-
-export const SearchItemImage = styled.div`
-	width: 12rem;
-	height: 100%;
-
-	img {
-		width: 100%;
-		height: 100%;
-	}
+	cursor: pointer;
 `;
 
 export const SearchItemText = styled.div`
-	min-height: 9rem;
 	margin-left: 1rem;
 	padding-top: 0.5rem;
 	flex: 1;
@@ -56,9 +23,10 @@ export const SearchItemText = styled.div`
 `;
 
 export const StyledStats = styled(Stats)`
-	background-color: ${({ theme }) => theme.colors.primary};
-	color: ${({ theme }) => theme.colors.white};
-	text-align: center;
-	margin-top: -0.5rem;
-	font-size: 1.8rem;
+	${({ theme: { colors } }) => css`
+		background-color: ${colors.primary};
+		color: ${colors.white};
+		text-align: center;
+		font-size: 1.8rem;
+	`}
 `;
