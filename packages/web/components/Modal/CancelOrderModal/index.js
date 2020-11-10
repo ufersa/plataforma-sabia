fd74d8e79939f47f7602ac1c0fd47b3e3e9fffc9
luizeboli <unifelicio@gmail.com>
2020-11-10 10:41:48 -0300

    refactor(web/order): organize code styles

--- packages/web/components/Modal/CancelOrderModal/index.js
+++ packages/web/components/Modal/CancelOrderModal/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
 import { useForm } from 'react-hook-form';
 import { TextField } from '../../Form';
 import { ReviewButton as Button } from '../CurateTechnologyModal/styles';
 import { toast } from '../../Toast';
-
-const customTextFieldCss = css`
-	${({ theme: { colors } }) => css`
-		> label {
-			display: flex;
-			font-size: 1.6rem;
-			line-height: 2.4rem;
-			color: ${colors.silver};
-			margin-top: 0.8rem;
-			margin-bottom: 1.6rem;
-		}
-	`}
-`;
+import { Modal, InfosContainer, customTextFieldCss } from './styles';
 
 const CancelOrderModal = ({ closeModal }) => {
 	const form = useForm();
@@ -58,51 +45,6 @@ const CancelOrderModal = ({ closeModal }) => {
 		</Modal>
 	);
 };
-
-const InfosContainer = styled.div`
-	${({ theme: { colors } }) => css`
-		display: flex;
-		flex-direction: column;
-
-		> p {
-			color: ${colors.red};
-			font-size: 2.8rem;
-			font-weight: 500;
-			margin-bottom: 0.8rem;
-			line-height: 3.3rem;
-		}
-
-		> span {
-			color: ${colors.silver};
-			font-size: 1.6rem;
-			line-height: 2.4rem;
-		}
-	`}
-`;
-
-const Modal = styled.form`
-	${({ theme: { screens } }) => css`
-		display: flex;
-		max-width: 77rem;
-
-		> div:first-child {
-			max-width: 26rem;
-		}
-
-		@media screen and (max-width: ${screens.medium}px) {
-			flex-direction: column;
-
-			> div:first-child {
-				display: none;
-			}
-
-			${InfosContainer} {
-				margin-left: 0;
-				margin-right: 0;
-			}
-		}
-	`}
-`;
 
 CancelOrderModal.propTypes = {
 	closeModal: PropTypes.func.isRequired,
---
+++ packages/web/components/Modal/CancelOrderModal/styles.js
@@ -0,0 +1,59 @@
+import styled, { css } from 'styled-components';
+
+export const InfosContainer = styled.div`
+	${({ theme: { colors } }) => css`
+		display: flex;
+		flex-direction: column;
+
+		> p {
+			color: ${colors.red};
+			font-size: 2.8rem;
+			font-weight: 500;
+			margin-bottom: 0.8rem;
+			line-height: 3.3rem;
+		}
+
+		> span {
+			color: ${colors.silver};
+			font-size: 1.6rem;
+			line-height: 2.4rem;
+		}
+	`}
+`;
+
+export const Modal = styled.form`
+	${({ theme: { screens } }) => css`
+		display: flex;
+		max-width: 77rem;
+
+		> div:first-child {
+			max-width: 26rem;
+		}
+
+		@media screen and (max-width: ${screens.medium}px) {
+			flex-direction: column;
+
+			> div:first-child {
+				display: none;
+			}
+
+			${InfosContainer} {
+				margin-left: 0;
+				margin-right: 0;
+			}
+		}
+	`}
+`;
+
+export const customTextFieldCss = css`
+	${({ theme: { colors } }) => css`
+		> label {
+			display: flex;
+			font-size: 1.6rem;
+			line-height: 2.4rem;
+			color: ${colors.silver};
+			margin-top: 0.8rem;
+			margin-bottom: 1.6rem;
+		}
+	`}
+`;
