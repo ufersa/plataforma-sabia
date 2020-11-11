f19c6a5f2db2302fec06a115e9aeeadbb9369584
NaylsonFerreira <naylsonfsa@gmail.com>
2020-11-11 13:34:52 -0300

    AboutForm

--- packages/admin/src/pages/technologies/Form/index.js
+++ packages/admin/src/pages/technologies/Form/index.js
@@ -1,45 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-	SimpleForm,
-	TextInput,
-	NumberInput,
-	BooleanInput,
-	TabbedShowLayout,
-	Tab,
-} from 'react-admin';
-
-import { TechnologyTermsSelect } from '../../../components';
+import { TabbedShowLayout, Tab } from 'react-admin';
 import StatusForm from './statusForm';
 import CostForm from './CostForm';
 import AttachmentsForm from './AttachmentsForm';
 import MapForm from './MapForm';
 import ResponsibleForm from './ResponsibleForm';
+import AboutForm from './AboutForm';
 
 const TechnologiesForm = ({ record, resource, save }) => {
+	if (!record.id) return <AboutForm record={record} save={save} resource={resource} />;
 	return (
 		<TabbedShowLayout record={record} resource={resource}>
 			<Tab label="About" path="">
-				<SimpleForm record={record} save={save} resource={resource}>
-					<TextInput source="title" fullWidth resettable />
-					<TextInput source="description" fullWidth resettable />
-					<BooleanInput source="private" />
-					<NumberInput source="thumbnail_id" fullWidth />
-					<NumberInput source="likes" fullWidth />
-					<BooleanInput source="patent" defaultValue />
-					<TextInput source="patent_number" fullWidth resettable />
-					<TextInput source="primary_purpose" fullWidth resettable multiline />
-					<TextInput source="secondary_purpose" fullWidth resettable multiline />
-					<TextInput source="application_mode" fullWidth resettable multiline />
-					<TextInput source="application_examples" fullWidth resettable multiline />
-					<NumberInput source="installation_time" fullWidth />
-					<TextInput source="solves_problem" fullWidth resettable multiline />
-					<TextInput source="entailes_problem" fullWidth resettable multiline />
-					<TextInput source="requirements" fullWidth resettable multiline />
-					<TextInput source="risks" fullWidth resettable multiline />
-					<TextInput source="contribution" fullWidth resettable multiline />
-					<TechnologyTermsSelect record={record} />
-				</SimpleForm>
+				<AboutForm record={record} save={save} resource={resource} />
 			</Tab>
 			<Tab label="Status" path="status">
 				<StatusForm record={record} resource={resource} />
@@ -67,7 +41,7 @@ TechnologiesForm.propTypes = {
 };
 
 TechnologiesForm.defaultProps = {
-	record: { id: null },
+	record: { id: 0 },
 	resource: '',
 	save: () => {},
 };
