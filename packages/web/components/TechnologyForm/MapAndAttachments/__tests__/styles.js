82ce580bffa3bc9a5406816262df14ac7e809b92
Davi de Oliveira <davioliveira.dev@gmail.com>
2020-11-13 16:38:20 -0300

    Add more tests

--- packages/web/components/TechnologyForm/MapAndAttachments/__tests__/styles.js
+++ packages/web/components/TechnologyForm/MapAndAttachments/__tests__/styles.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { render, screen } from 'test-utils';
-import { VideosWrapper, VideoItem } from '../styles';
+import {
+	ButtonVideoAdd,
+	VideoContainer,
+	VideosWrapper,
+	VideoItem,
+	RemoveVideoButton,
+} from '../styles';
 
 const video = {
 	thumbnail: 'http://i3.ytimg.com/vi/8h7p88oySWY/hqdefault.jpg',
 	videoId: '8h7p88oySWY',
 };
 
+const onAdd = jest.fn(() => {});
+const onRemove = jest.fn(() => {});
+
+test('it render the <VideoContainer /> component', () => {
+	const { container } = render(<VideoContainer />);
+
+	expect(container).toMatchSnapshot();
+});
+
 test('it render the <VideosWrapper /> component', () => {
 	const { container } = render(<VideosWrapper />);
 
+	expect(container).toMatchSnapshot();
+});
+
+test('it render the <ButtonVideoAdd /> component', () => {
+	const { container } = render(
+		<ButtonVideoAdd type="button" variant="secondary" onClick={onAdd}>
+			Adicionar
+		</ButtonVideoAdd>,
+	);
+	
+	expect(screen.getByRole('button', { name: /adicionar/i })).toBeEnabled();
 	expect(container).toMatchSnapshot();
 });
 
@@ -19,9 +45,13 @@ test('it render the <VideoItem /> component', async () => {
 			<a href={`//www.youtube.com/watch?v=${video.videoId}`} target="_blank" rel="noreferrer">
 				<img src={video.thumbnail} alt={`Youtube vídeo ${video.videoId}`} />
 			</a>
+			<RemoveVideoButton type="button" onClick={onRemove}>
+				Remover
+			</RemoveVideoButton>
 		</VideoItem>,
 	);
 
 	expect(screen.getByRole('img', { src: video.thumbnail })).toBeInTheDocument();
+	expect(screen.getByRole('button', { name: /remover/i })).toBeEnabled();
 	expect(container).toMatchSnapshot();
 });
