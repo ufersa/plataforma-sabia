f0d2e25ae68574e11f58b4aad976be3fca0092c6
Davi de Oliveira <davioliveira.dev@gmail.com>
2020-11-13 13:58:15 -0300

    Add styles tests

---
+++ packages/web/components/TechnologyForm/MapAndAttachments/__tests__/styles.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen } from 'test-utils';
+import { VideosWrapper, VideoItem } from '../styles';
+
+const video = {
+  thumbnail: 'http://i3.ytimg.com/vi/8h7p88oySWY/hqdefault.jpg',
+  videoId: '8h7p88oySWY',
+};
+
+test('it render the <VideosWrapper /> component', () => {
+	const { container } = render(
+		<VideosWrapper />,
+	);
+
+	expect(container).toMatchSnapshot();
+});
+
+test('it render the <VideoItem /> component', async () => {
+	const { container } = render(
+		<VideoItem>
+      <a
+        href={`//www.youtube.com/watch?v=${video.videoId}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={video.thumbnail}
+          alt={`Youtube vídeo ${video.videoId}`}
+        />
+      </a>
+    </VideoItem>,
+	);
+
+  expect(screen.getByRole('img', { src: video.thumbnail })).toBeInTheDocument();
+	expect(container).toMatchSnapshot();
+});
