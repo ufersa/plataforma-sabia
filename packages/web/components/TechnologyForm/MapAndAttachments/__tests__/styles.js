38b276056b1966eece10ca625247161809200a74
Davi de Oliveira <davioliveira.dev@gmail.com>
2020-11-13 14:04:09 -0300

    Lint errors

--- packages/web/components/TechnologyForm/MapAndAttachments/__tests__/styles.js
+++ packages/web/components/TechnologyForm/MapAndAttachments/__tests__/styles.js
@@ -3,14 +3,12 @@ import { render, screen } from 'test-utils';
 import { VideosWrapper, VideoItem } from '../styles';
 
 const video = {
-  thumbnail: 'http://i3.ytimg.com/vi/8h7p88oySWY/hqdefault.jpg',
-  videoId: '8h7p88oySWY',
+	thumbnail: 'http://i3.ytimg.com/vi/8h7p88oySWY/hqdefault.jpg',
+	videoId: '8h7p88oySWY',
 };
 
 test('it render the <VideosWrapper /> component', () => {
-	const { container } = render(
-		<VideosWrapper />,
-	);
+	const { container } = render(<VideosWrapper />);
 
 	expect(container).toMatchSnapshot();
 });
@@ -18,19 +16,12 @@ test('it render the <VideosWrapper /> component', () => {
 test('it render the <VideoItem /> component', async () => {
 	const { container } = render(
 		<VideoItem>
-      <a
-        href={`//www.youtube.com/watch?v=${video.videoId}`}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img
-          src={video.thumbnail}
-          alt={`Youtube vídeo ${video.videoId}`}
-        />
-      </a>
-    </VideoItem>,
+			<a href={`//www.youtube.com/watch?v=${video.videoId}`} target="_blank" rel="noreferrer">
+				<img src={video.thumbnail} alt={`Youtube vídeo ${video.videoId}`} />
+			</a>
+		</VideoItem>,
 	);
 
-  expect(screen.getByRole('img', { src: video.thumbnail })).toBeInTheDocument();
+	expect(screen.getByRole('img', { src: video.thumbnail })).toBeInTheDocument();
 	expect(container).toMatchSnapshot();
 });
