0cbab564acee7eab0a0ea4f070e90b423f8c2f48
Lorena Calaça <lorena.calaca.od@gmail.com>
2020-11-14 18:00:13 -0300

    feat(web): remove install column, add likes and format status

--- packages/web/pages/user/my-account/technologies.js
+++ packages/web/pages/user/my-account/technologies.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import { FiPlus } from 'react-icons/fi';
 import Link from 'next/link';
@@ -9,7 +9,25 @@ import { UserProfile } from '../../../components/UserProfile';
 import { DataGrid } from '../../../components/DataGrid';
 import { getUserTechnologies } from '../../../services';
 import { Title } from '../../../components/Common';
-import { getPeriod } from '../../../utils/helper';
+import { STATUS as technologyStatusEnum } from '../../../utils/enums/technology.enums';
+
+/**
+ * Returns technology status text based on status key
+ *
+ * @param {string} value The status key
+ * @returns {string} Status text
+ */
+export const getTechnologyStatusText = (value) =>
+	({
+		[technologyStatusEnum.DRAFT]: 'Rascunho',
+		[technologyStatusEnum.PENDING]: 'Pendente',
+		[technologyStatusEnum.IN_REVIEW]: 'Em Análise',
+		[technologyStatusEnum.REQUESTED_CHANGES]: 'Correção Solicitada',
+		[technologyStatusEnum.CHANGES_MADE]: 'Correção Feita',
+		[technologyStatusEnum.APPROVED]: 'Aprovada',
+		[technologyStatusEnum.REJECTED]: 'Rejeitada',
+		[technologyStatusEnum.PUBLISHED]: 'Publicada',
+	}[value]);
 
 const MyTechnologies = ({ technologies }) => {
 	const { t } = useTranslation(['helper', 'account']);
@@ -37,14 +55,16 @@ const MyTechnologies = ({ technologies }) => {
 								</Stats>
 							</InfoContainer>
 							<DataGrid
-								data={technologies.map(
-									({ id, title, status, installation_time }) => ({
-										id,
-										Título: title,
-										Status: status,
-										'Tempo de implantação': getPeriod(t, installation_time),
-									}),
-								)}
+								data={technologies.map(({ id, title, status, likes }) => ({
+									id,
+									Título: title,
+									Status: (
+										<DealStatus status={status}>
+											{getTechnologyStatusText(status)}
+										</DealStatus>
+									),
+									Favoritos: likes,
+								}))}
 								rowLink="/technology/:id/edit"
 							/>
 						</MainContent>
@@ -143,6 +163,83 @@ export const Stats = styled.span`
 export const NoTechnologies = styled.span`
 	color: ${({ theme }) => theme.colors.darkGray};
 	font-size: 2rem;
+`;
+
+const statusModifiers = {
+	[technologyStatusEnum.DRAFT]: (colors) => css`
+		color: ${colors.lightGray2};
+		&::before {
+			background: ${colors.lightGray2};
+		}
+	`,
+	[technologyStatusEnum.PENDING]: (colors) => css`
+		color: ${colors.primary};
+		&::before {
+			background: ${colors.primary};
+		}
+	`,
+	[technologyStatusEnum.IN_REVIEW]: (colors) => css`
+		color: ${colors.lightBlue};
+		&::before {
+			background: ${colors.lightBlue};
+		}
+	`,
+	[technologyStatusEnum.REQUESTED_CHANGES]: (colors) => css`
+		color: ${colors.darkOrange};
+		&::before {
+			background: ${colors.darkOrange};
+		}
+	`,
+	[technologyStatusEnum.CHANGES_MADE]: (colors) => css`
+		color: ${colors.blue};
+		&::before {
+			background: ${colors.blue};
+		}
+	`,
+	[technologyStatusEnum.APPROVED]: (colors) => css`
+		color: ${colors.secondary};
+		&::before {
+			background: ${colors.secondary};
+		}
+	`,
+	[technologyStatusEnum.REJECTED]: (colors) => css`
+		color: ${colors.red};
+		&::before {
+			background: ${colors.red};
+		}
+	`,
+	[technologyStatusEnum.PUBLISHED]: (colors) => css`
+		color: ${colors.darkGreen};
+		&::before {
+			background: ${colors.darkGreen};
+		}
+	`,
+};
+
+export const DealStatus = styled.div`
+	${({ theme: { colors }, status }) => css`
+		display: inline-block;
+		position: relative;
+		line-height: 2.4rem;
+		font-weight: 500;
+		padding: 0.2rem 0.8rem;
+		max-width: fit-content;
+		text-align: center;
+
+		&::before {
+			content: '';
+			display: block;
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			border-radius: 1.45rem;
+			opacity: 0.1;
+		}
+
+		${!!status && statusModifiers[status](colors)};
+	`}
 `;
 
 export default MyTechnologies;
