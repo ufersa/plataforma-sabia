cac2047279fe1ef041eb518246fdf5011f0491e0
NaylsonFerreira <naylsonfsa@gmail.com>
2020-11-14 11:36:55 -0300

    removed 'Veja mais' in DisclaimerSeeder

--- packages/api/database/seeds/10_DisclaimerSeeder.js
+++ packages/api/database/seeds/10_DisclaimerSeeder.js
@@ -23,21 +23,21 @@ class DisclaimerSeeder {
 			},
 			{
 				description:
-					'Concordo com o processamento dos meus dados pessoais para fins de fornecimento dos serviços da Plataforma Sabiá. Veja mais na Política de Privacidade. ',
+					'Concordo com o processamento dos meus dados pessoais para fins de fornecimento dos serviços da Plataforma Sabiá.  ',
 				required: true,
 				type: 'termsOfUseRegister',
 				version: '1',
 			},
 			{
 				description:
-					'Concordo em respeitar a legislação brasileira vigente no conteúdo que eu venha a disponibilizar na Plataforma Sabiá, sendo de minha exclusiva responsabilidade. Veja mais nos Termos e Condições de Uso. ',
+					'Concordo em respeitar a legislação brasileira vigente no conteúdo que eu venha a disponibilizar na Plataforma Sabiá, sendo de minha exclusiva responsabilidade.  ',
 				required: true,
 				type: 'termsOfUseRegister',
 				version: '1',
 			},
 			{
 				description:
-					'Estou ciente de que posso revogar o consentimento de uso dos meus dados pessoais a qualquer momento. Todavia, não poderei mais utilizar os serviços da plataforma que necessitam do uso e da coleta de dados pessoais. Veja mais na Política de Privacidade. ',
+					'Estou ciente de que posso revogar o consentimento de uso dos meus dados pessoais a qualquer momento. Todavia, não poderei mais utilizar os serviços da plataforma que necessitam do uso e da coleta de dados pessoais.  ',
 				required: true,
 				type: 'termsOfUseRegister',
 				version: '1',
@@ -51,42 +51,42 @@ class DisclaimerSeeder {
 			},
 			{
 				description:
-					'Estou ciente que em hipótese alguma será constituído vínculo cooperativo, associativo, societário ou empregatício entre a plataforma, os usuários cadastrados e os parceiros. Veja mais nos Termos e Condições de Uso.',
+					'Estou ciente que em hipótese alguma será constituído vínculo cooperativo, associativo, societário ou empregatício entre a plataforma, os usuários cadastrados e os parceiros. ',
 				required: true,
 				type: 'termsOfUseRegister',
 				version: '1',
 			},
 			{
 				description:
-					'Concordo com o processamento dos meus dados pessoais com o objetivo de receber publicidade da Plataforma Sabiá e de terceiros parceiros. Veja mais nos Termos e Condições de Uso.',
+					'Concordo com o processamento dos meus dados pessoais com o objetivo de receber publicidade da Plataforma Sabiá e de terceiros parceiros. ',
 				required: true,
 				type: 'termsOfUseRegister',
 				version: '1',
 			},
 			{
 				description:
-					'Declaro ciência de que devo fornecer apenas informações verdadeiras no cadastro das tecnologias. Veja mais nos Termos e Condições de Uso.',
+					'Declaro ciência de que devo fornecer apenas informações verdadeiras no cadastro das tecnologias. ',
 				required: true,
 				type: 'termsOfUseTechnology',
 				version: '1',
 			},
 			{
 				description:
-					'Estou ciente de que as informações cadastradas são de minha inteira responsabilidade, e a Plataforma Sabiá não responderá por quaisquer violações ao Direito de Propriedade Intelectual e Direito Autoral de terceiros. Veja mais nos Termos e Condições de Uso.',
+					'Estou ciente de que as informações cadastradas são de minha inteira responsabilidade, e a Plataforma Sabiá não responderá por quaisquer violações ao Direito de Propriedade Intelectual e Direito Autoral de terceiros. ',
 				required: true,
 				type: 'termsOfUseTechnology',
 				version: '1',
 			},
 			{
 				description:
-					'Estou ciente de que poderei ser penalizado com advertência, suspensão e encerramento da minha conta por eventuais violações a direitos de terceiros no cadastro das tecnologias, como o Direito de Propriedade Intelectual e Direito Autoral. Veja mais nos Termos e Condições de Uso.',
+					'Estou ciente de que poderei ser penalizado com advertência, suspensão e encerramento da minha conta por eventuais violações a direitos de terceiros no cadastro das tecnologias, como o Direito de Propriedade Intelectual e Direito Autoral. ',
 				required: true,
 				type: 'termsOfUseTechnology',
 				version: '1',
 			},
 			{
 				description:
-					'Declaro ciência de que as transgressões a direitos de terceiros no cadastro das tecnologias podem implicar em responsabilização na esfera jurisdicional cível e criminal. Veja mais nos Termos e Condições de Uso.',
+					'Declaro ciência de que as transgressões a direitos de terceiros no cadastro das tecnologias podem implicar em responsabilização na esfera jurisdicional cível e criminal. ',
 				required: true,
 				type: 'termsOfUseTechnology',
 				version: '1',
