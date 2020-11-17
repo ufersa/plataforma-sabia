899cdd5f8e3dc7b72d665e05cc3a8f1c9425d4ce
Luiz Cláudio <luizcns18@gmail.com>
2020-11-16 22:01:30 -0300

    Add initial structure

---
+++ packages/web/components/Maintenance/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Maintenance = () => {
+	return <Container>Página em Manutenção</Container>;
+};
+
+export default Maintenance;
---
+++ packages/web/components/Maintenance/styles.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+
+export const Container = styled.main`
+	min-height: 80vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	> img {
+		margin-left: 8.8rem;
+	}
+`;
