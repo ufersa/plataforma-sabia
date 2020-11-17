3ab6e6883e73b1aa67ddba9deb1e12f4031988c1
mateus4k <mateus4k@protonmail.ch>
2020-11-17 13:12:36 -0300

    testing

---
+++ packages/api/database/migrations/1605628575470_add_on_delete_to_technology_schema.js
@@ -0,0 +1,30 @@
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class AddOnDeleteToTechnologySchema extends Schema {
+	up() {
+		this.alter('technologies', (table) => {
+			table
+				.integer('thumbnail_id')
+				.unsigned()
+				.references('id')
+				.inTable('uploads')
+				.onUpdate('CASCADE')
+				.onDelete('SET NULL')
+				.alter();
+		});
+	}
+
+	down() {
+		this.alter('technologies', (table) => {
+			table
+				.integer('thumbnail_id')
+				.unsigned()
+				.references('id')
+				.inTable('uploads')
+				.alter();
+		});
+	}
+}
+
+module.exports = AddOnDeleteToTechnologySchema;
