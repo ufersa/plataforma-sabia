4f6008b2bd031c67a91835a52531a8ec3d4f43c9
Alexandre Pontes <alexandre.adames@gmail.com>
2020-11-19 20:49:53 -0300

    sendMailToBuyer as a Bull process

--- packages/api/app/Controllers/Http/TechnologyOrderController.js
+++ packages/api/app/Controllers/Http/TechnologyOrderController.js
@@ -1,35 +1,10 @@
 const { orderStatuses, errorPayload, errors } = require('../../Utils');
 
-const Config = use('Adonis/Src/Config');
-const Mail = require('../../Utils/mail');
+const Bull = use('Rocketseat/Bull');
+const Job = use('App/Jobs/SendMail');
 
 const TechnologyOrder = use('App/Models/TechnologyOrder');
 class TechnologyOrderController {
-	async sendEmailToBuyer(order, antl) {
-		const buyer = await order.user().first();
-		const technology = await order.technology().first();
-		const { from } = Config.get('mail');
-		const unitValueFormated = new Intl.NumberFormat('pt-BR', {
-			style: 'currency',
-			currency: 'BRL',
-		}).format(order.unit_value);
-		const { quantity } = order;
-		try {
-			await Mail.send(
-				'emails.technology-order-closed',
-				{ buyer, technology, quantity, unitValueFormated },
-				(message) => {
-					message.subject(antl('message.order.technologyOrderClosed'));
-					message.from(from);
-					message.to(buyer.email);
-				},
-			);
-		} catch (exception) {
-			// eslint-disable-next-line no-console
-			console.error(exception);
-		}
-	}
-
 	/**
 	 * Close TechnologyOrder.
 	 * PUT orders/:id/close
@@ -51,7 +26,23 @@ class TechnologyOrderController {
 		order.status = orderStatuses.CLOSED;
 		order.merge(data);
 		await order.save();
-		await this.sendEmailToBuyer(order, request.antl);
+		const buyer = await order.user().first();
+		const technology = await order.technology().first();
+		const unitValueFormated = new Intl.NumberFormat('pt-BR', {
+			style: 'currency',
+			currency: 'BRL',
+		}).format(order.unit_value);
+		const { quantity } = order;
+		const mailData = {
+			email: buyer.email,
+			subject: request.antl('message.order.technologyOrderClosed'),
+			template: 'emails.technology-order-closed',
+			buyer,
+			technology,
+			quantity,
+			unitValueFormated,
+		};
+		Bull.add(Job.key, mailData, { attempts: 3 });
 		return order.toJSON();
 	}
 }
