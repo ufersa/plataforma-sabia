1750b92aded7e744134d2ba99fde0983744658ce
Luiz Cláudio <luizcns18@gmail.com>
2020-11-21 12:53:04 -0300

    Improve maintenance pages

--- packages/web/components/Maintenance/index.js
+++ packages/web/components/Maintenance/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
-import { Container } from './styles';
+import { Container, Button } from './styles';
 
 const Maintenance = () => {
-	return <Container>Página em Manutenção</Container>;
+	const router = useRouter();
+	return (
+		<Container>
+			<img
+				src="building-blocks-rafiki.svg"
+				alt="Ilustração de um rapaz em pé segurando uma caixa laranja em frente a algumas caixas verdes empilhadas"
+			/>
+			<h1>Oops, esta página ainda não está disponível</h1>
+			<p>
+				Enquanto nós trabalhamos nessa funcionalidade, fique a vontade para voltar e
+				usufruir de tudo que nossa plataforma oferece para você hoje.
+			</p>
+			<Button type="button" variant="success" onClick={() => router.push('/')}>
+				Voltar para a página principal
+			</Button>
+		</Container>
+	);
 };
 
 export default Maintenance;
