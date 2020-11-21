9e84516caca450f189e70e4c6ca115ca2b532fa5
Luiz Cláudio <luizcns18@gmail.com>
2020-11-21 16:54:21 -0300

    Update fonts structure

--- packages/web/pages/_document.js
+++ packages/web/pages/_document.js
@@ -1,8 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import 'isomorphic-fetch';
+import GoogleFonts from 'next-google-fonts';
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx) {
@@ -28,5 +29,19 @@ export default class MyDocument extends Document {
 		} finally {
 			sheet.seal();
 		}
+	}
+
+	render() {
+		return (
+			<Html lang="pt-BR">
+				<GoogleFonts href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap" />
+				<GoogleFonts href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&family=Rubik:wght@400;500&display=swap" />
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
 	}
 }
