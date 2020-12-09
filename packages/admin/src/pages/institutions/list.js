61a500d75b015b2ca159758f628892e7593373b0
NaylsonFerreira <naylsonfsa@gmail.com>
2020-12-09 11:29:47 -0300

    Crud Institutions

---
+++ packages/admin/src/pages/institutions/list.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+	List,
+	Datagrid,
+	TextField,
+	ReferenceField,
+	EditButton,
+	DeleteWithConfirmButton,
+} from 'react-admin';
+
+const InstitutionsList = ({ basePath, resource, hasCreate, hasEdit, hasList, hasShow }) => (
+	<List
+		basePath={basePath}
+		resource={resource}
+		hasCreate={hasCreate}
+		hasEdit={hasEdit}
+		hasList={hasList}
+		hasShow={hasShow}
+		perPage={25}
+	>
+		<Datagrid>
+			<TextField source="id" />
+			<TextField source="name" />
+			<TextField source="initials" />
+			<TextField source="city" />
+			<TextField source="state" />
+			<TextField source="cnpj" />
+			<ReferenceField source="user_id" reference="users">
+				<TextField source="email" />
+			</ReferenceField>
+			<EditButton />
+			<DeleteWithConfirmButton />
+		</Datagrid>
+	</List>
+);
+InstitutionsList.propTypes = {
+	resource: PropTypes.string.isRequired,
+	basePath: PropTypes.string.isRequired,
+	hasCreate: PropTypes.bool.isRequired,
+	hasEdit: PropTypes.bool.isRequired,
+	hasList: PropTypes.bool.isRequired,
+	hasShow: PropTypes.bool.isRequired,
+};
+
+export default InstitutionsList;
