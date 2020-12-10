c1efc241608888dc9ef204fb3c755a14591eb341
Alexandre Pontes <alexandre.adames@gmail.com>
2020-12-10 14:09:55 -0300

    Read Message, mark as read and mark as new

--- packages/api/app/Controllers/Http/MessageController.js
+++ packages/api/app/Controllers/Http/MessageController.js
@@ -1,4 +1,6 @@
 const Message = use('App/Models/Message');
+const { messageStatuses } = require('../../Utils');
+
 class MessageController {
 	async index({ auth, request }) {
 		const filters = request.all();
@@ -7,6 +9,36 @@ class MessageController {
 			.where({ user_id: user.id })
 			.withFilters(filters)
 			.withParams(request);
+	}
+
+	async show({ auth, request }) {
+		const user = await auth.getUser();
+		const message = await Message.query()
+			.where({ user_id: user.id })
+			.withParams(request);
+		message.status = messageStatuses.READ;
+		await message.save();
+		return message;
+	}
+
+	async markAsRead({ auth, request }) {
+		const { messages } = request.all();
+		const user = await auth.getUser();
+		const messagesMarkedAsRead = await Message.query()
+			.where({ user_id: user.id })
+			.update({ status: messageStatuses.READ })
+			.whereIn('id', messages);
+		return messagesMarkedAsRead;
+	}
+
+	async markAsNew({ auth, request }) {
+		const { messages } = request.all();
+		const user = await auth.getUser();
+		const messagesMarkedAsRead = await Message.query()
+			.where({ user_id: user.id })
+			.update({ status: messageStatuses.NEW })
+			.whereIn('id', messages);
+		return messagesMarkedAsRead;
 	}
 }
 
--- packages/api/start/routes/messages.js
+++ packages/api/start/routes/messages.js
@@ -1,3 +1,10 @@
 const Route = use('Route');
 
 Route.get('/messages', 'MessageController.index').middleware(['auth', 'handleParams']);
+Route.get('/messages/:id', 'MessageController.show').middleware(['auth', 'handleParams']);
+Route.put('/messages/mark-as-read', 'MessageController.markAsRead')
+	.middleware(['auth'])
+	.validator('markMessages');
+Route.put('/messages/mark-as-new', 'MessageController.markAsNew')
+	.middleware(['auth'])
+	.validator('markMessages');
