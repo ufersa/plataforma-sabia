d6296b0c4185496e9ab9b8bf4fb544773962b2e1
mateus4k <mateus4k@protonmail.ch>
2020-12-16 18:08:34 -0300

    WIP

--- packages/web/pages/user/my-account/technologies.js
+++ packages/web/pages/user/my-account/technologies.js
@@ -1,23 +1,36 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
+import useSWR from 'swr';
 import styled, { css } from 'styled-components';
-import { useTranslation } from 'react-i18next';
-import { FiX, FiPlus, FiEdit, FiCheck } from 'react-icons/fi';
 import Link from 'next/link';
+import { useTranslation } from 'react-i18next';
 import { useRouter } from 'next/router';
+import { FiX, FiPlus, FiEdit, FiCheck } from 'react-icons/fi';
 import { Protected } from '../../../components/Authorization';
 import { UserProfile } from '../../../components/UserProfile';
 import { DataGrid } from '../../../components/DataGrid';
 import { Title } from '../../../components/Common';
 import { IconButton } from '../../../components/Button';
-import { getUserTechnologies } from '../../../services';
+import Loading from '../../../components/Loading';
+import { getUserTechnologies, updateTechnologyActiveStatus } from '../../../services';
 import { getPeriod } from '../../../utils/helper';
 import { useModal } from '../../../hooks';
 
-const MyTechnologies = ({ technologies }) => {
+const MyTechnologies = ({ initialTechnologies }) => {
+	const [focused, setFocused] = useState(null);
 	const { t } = useTranslation(['helper', 'account']);
 	const { openModal } = useModal();
 	const router = useRouter();
+
+	// eslint-disable-next-line no-unused-vars
+	const { data: technologies = [], isValidating, revalidate, mutate } = useSWR(
+		['updateTechnologyActiveStatus', focused?.id],
+		(_, id) => updateTechnologyActiveStatus(id),
+		{
+			initialData: initialTechnologies,
+			// revalidateOnMount: true,
+		},
+	);
 
 	const handleActive = useCallback(
 		(data) => {
@@ -29,12 +42,13 @@ const MyTechnologies = ({ technologies }) => {
 				title: active
 					? 'Deseja desativar esta tecnologia?'
 					: 'Deseja ativar esta tecnologia?',
-				onSubmit: (technology) => {
-					console.log(technology);
+				onSubmit: async (technology) => {
+					await setFocused(technology);
+					revalidate();
 				},
 			});
 		},
-		[openModal],
+		[openModal, revalidate],
 	);
 
 	const handleEditClick = useCallback((id) => router.push(`/technology/${id}/edit`), [router]);
@@ -47,60 +61,67 @@ const MyTechnologies = ({ technologies }) => {
 					<Title align="left" noPadding noMargin>
 						{t('account:titles.myTechnologies')}
 					</Title>
-					{technologies.length > 0 ? (
-						<MainContent>
-							<InfoContainer>
-								<Link href="/technology/new">
-									<AddButton>
-										<span>{t('account:labels.addTechnologies')}</span>
-										<FiPlus />
-									</AddButton>
-								</Link>
-								<Stats>
-									{t('account:labels.registeredTechnologies', {
-										count: technologies.length,
+
+					<Loading loading={isValidating}>
+						{technologies.length > 0 ? (
+							<MainContent>
+								<InfoContainer>
+									<Link href="/technology/new">
+										<AddButton>
+											<span>{t('account:labels.addTechnologies')}</span>
+											<FiPlus />
+										</AddButton>
+									</Link>
+									<Stats>
+										{t('account:labels.registeredTechnologies', {
+											count: technologies.length,
+										})}
+									</Stats>
+								</InfoContainer>
+								<DataGrid
+									data={technologies.map((technology) => {
+										return {
+											id: technology.id,
+											Título: technology.title,
+											Status: technology.status,
+											'Tempo de implantação': getPeriod(
+												t,
+												technology.installation_time,
+											),
+											Ações: (
+												<DealActions>
+													<IconButton
+														variant="info"
+														aria-label="Edit Technology"
+														onClick={() =>
+															handleEditClick(technology.id)
+														}
+													>
+														<FiEdit />
+													</IconButton>
+													<IconButton
+														variant={
+															technology.active ? 'remove' : 'success'
+														}
+														aria-label={`${
+															technology.active ? 'Enable' : 'Disable'
+														} Technology`}
+														onClick={() => handleActive(technology)}
+													>
+														{technology.active ? <FiX /> : <FiCheck />}
+													</IconButton>
+												</DealActions>
+											),
+										};
 									})}
-								</Stats>
-							</InfoContainer>
-							<DataGrid
-								data={technologies.map((technology) => {
-									return {
-										id: technology.id,
-										Título: technology.title,
-										Status: technology.status,
-										'Tempo de implantação': getPeriod(
-											t,
-											technology.installation_time,
-										),
-										Ações: (
-											<DealActions>
-												<IconButton
-													variant="info"
-													aria-label="Edit Technology"
-													onClick={() => handleEditClick(technology.id)}
-												>
-													<FiEdit />
-												</IconButton>
-												<IconButton
-													variant={
-														technology.active ? 'remove' : 'success'
-													}
-													aria-label={`${
-														technology.active ? 'Enable' : 'Disable'
-													} Technology`}
-													onClick={() => handleActive(technology)}
-												>
-													{technology.active ? <FiX /> : <FiCheck />}
-												</IconButton>
-											</DealActions>
-										),
-									};
-								})}
-							/>
-						</MainContent>
-					) : (
-						<NoTechnologies>{t('account:messages.noTechnologyToShow')}</NoTechnologies>
-					)}
+								/>
+							</MainContent>
+						) : (
+							<NoTechnologies>
+								{t('account:messages.noTechnologyToShow')}
+							</NoTechnologies>
+						)}
+					</Loading>
 				</MainContentContainer>
 			</Protected>
 		</Container>
@@ -108,7 +129,7 @@ const MyTechnologies = ({ technologies }) => {
 };
 
 MyTechnologies.propTypes = {
-	technologies: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+	initialTechnologies: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 MyTechnologies.getInitialProps = async (ctx) => {
