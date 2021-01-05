4206725bff154cdad258fabba584fd0bc4bf8486
NaylsonFerreira <naylsonfsa@gmail.com>
2021-01-05 19:04:54 -0300

    improving syncronizeTerms

--- packages/api/app/Controllers/Http/AnnouncementController.js
+++ packages/api/app/Controllers/Http/AnnouncementController.js
@@ -69,27 +69,17 @@ class AnnouncementController {
 
 	async syncronizeTerms(trx, terms, announcement, detach = false) {
 		const termInstances = await Promise.all(terms.map((term) => Term.getTerm(term)));
+		const termInstancesIds = termInstances.map((term) => term.id);
 		if (detach) {
 			const taxonomyIds = termInstances.map((term) => term.taxonomy_id);
-			const announcementTerms = await Term.query()
-				.whereHas('announcements', (builder) => {
-					builder.where('id', announcement.id);
-				})
+			await announcement
+				.terms()
 				.whereIn('taxonomy_id', taxonomyIds)
-				.fetch();
-
-			const announcementTermsIds = announcementTerms
-				? announcementTerms.rows.map((announcementTerm) => announcementTerm.id)
-				: null;
-
-			await announcement.terms().detach(announcementTermsIds, null, trx);
+				.select('id')
+				.detach(null, trx);
 		}
 
-		await announcement.terms().attach(
-			termInstances.map((term) => term.id),
-			null,
-			trx,
-		);
+		await announcement.terms().attach(termInstancesIds, null, trx);
 	}
 
 	async store({ auth, request }) {
@@ -105,12 +95,8 @@ class AnnouncementController {
 			announcement = await Announcement.create(data, trx);
 			await announcement.user().associate(announcementOwner, trx);
 			await announcement.institution().associate(institution, trx);
-			if (targetAudiences) {
-				await this.syncronizeTerms(trx, targetAudiences, announcement);
-			}
-			if (keywords) {
-				await this.syncronizeTerms(trx, keywords, announcement);
-			}
+			const terms = [...targetAudiences, ...keywords];
+			await this.syncronizeTerms(trx, terms, announcement);
 			await announcement.loadMany(['institution', 'keywords', 'targetAudiences']);
 			await commit();
 		} catch (error) {
@@ -139,12 +125,8 @@ class AnnouncementController {
 				const institution = await Institution.findOrFail(institution_id);
 				await announcement.institution().associate(institution, trx);
 			}
-			if (targetAudiences) {
-				await this.syncronizeTerms(trx, targetAudiences, announcement, true);
-			}
-			if (keywords) {
-				await this.syncronizeTerms(trx, keywords, announcement, true);
-			}
+			const terms = [...targetAudiences, ...keywords];
+			await this.syncronizeTerms(trx, terms, announcement, true);
 			await announcement.loadMany(['institution', 'keywords', 'targetAudiences']);
 			await commit();
 		} catch (error) {
