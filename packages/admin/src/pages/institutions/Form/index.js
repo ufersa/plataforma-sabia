f1f64de5c04e63bc299b695382f8a139fc8361bb
Naylson Ferreira <naylsonfsa@gmail.com>
2021-01-15 11:28:39 -0300

    adding fields (#747)

--- packages/admin/src/pages/institutions/Form/index.js
+++ packages/admin/src/pages/institutions/Form/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { SimpleForm, TextInput, required, ReferenceInput, SelectInput } from 'react-admin';
+import {
+	SimpleForm,
+	TextInput,
+	required,
+	SelectInput,
+	NumberInput,
+	ReferenceInput,
+} from 'react-admin';
 
 const InstitutionsForm = ({ record, save, resource }) => (
 	<SimpleForm record={record} save={save} resource={resource}>
 		<TextInput source="name" fullWidth validate={[required()]} />
 		<TextInput source="initials" fullWidth validate={[required()]} />
+		<TextInput source="email" fullWidth validate={[required()]} />
+		<TextInput source="phone_number" fullWidth validate={[required()]} />
+		<TextInput source="website" fullWidth validate={[required()]} />
+		<NumberInput source="logo_id" fullWidth />
+		<SelectInput
+			source="type"
+			fullWidth
+			validate={[required()]}
+			choices={[
+				{ id: 'public', name: 'Public' },
+				{ id: 'private', name: 'Private' },
+				{ id: 'mixed', name: 'Mixed' },
+				{ id: 'other', name: 'Other' },
+			]}
+		/>
+		<SelectInput
+			source="category"
+			fullWidth
+			validate={[required()]}
+			choices={[
+				{ id: 'university', name: 'University' },
+				{ id: 'institute', name: 'Institute' },
+				{ id: 'association', name: 'Association' },
+				{ id: 'foundation', name: 'Foundation' },
+				{ id: 'cooperative', name: 'Cooperative' },
+				{ id: 'company', name: 'Company' },
+				{ id: 'other', name: 'Other' },
+			]}
+		/>
 		<TextInput source="state" fullWidth validate={[required()]} />
 		<TextInput source="city" fullWidth validate={[required()]} />
 		<TextInput source="address" fullWidth validate={[required()]} />
