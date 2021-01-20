b1ec33e679ff96e0fe2da289a64e8cab06394457
Mateus <mateus4k@protonmail.ch>
2021-01-20 15:59:33 -0300

    feat(algolia): add save many

--- packages/api/app/Utils/Algolia/indexes/service.js
+++ packages/api/app/Utils/Algolia/indexes/service.js
@@ -13,10 +13,18 @@ const prepareService = (service) => {
 /**
  * Index service to Algolia.
  *
- * @param {object} data Service data
+ * @param {object|object[]} data Service data
+ * @param {object} options Options passed
+ * @param {boolean} options.saveMany Save too many objects or just one
  */
-module.exports = async (data) => {
-	const { saveObject } = initIndex('service');
+module.exports = async (data, options = {}) => {
+	const { saveObjects, saveObject } = initIndex('service');
+
+	if (options.saveMany) {
+		const service = await data.map((idea) => prepareService(idea));
+		return saveObjects(service);
+	}
+
 	const service = await prepareService(data);
 	return saveObject(service);
 };
