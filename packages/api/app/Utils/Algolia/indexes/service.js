83548466948902d5586b0343d63551994f2df034
mateus4k <mateus4k@protonmail.ch>
2021-01-19 22:07:00 -0300

    refactor(service): remove unused option from indexes utils

--- packages/api/app/Utils/Algolia/indexes/service.js
+++ packages/api/app/Utils/Algolia/indexes/service.js
@@ -14,17 +14,9 @@ const prepareService = (service) => {
  * Index service to Algolia.
  *
  * @param {object|object[]} data Service data
- * @param {object} options Options passed
- * @param {boolean} options.saveMany Save too many objects or just one
  */
-module.exports = async (data, options = {}) => {
-	const { saveObjects, saveObject } = initIndex('service');
-
-	if (options.saveMany) {
-		const services = await data.map((service) => prepareService(service));
-		return saveObjects(services);
-	}
-
+module.exports = async (data) => {
+	const { saveObject } = initIndex('service');
 	const service = await prepareService(data);
 	return saveObject(service);
 };
