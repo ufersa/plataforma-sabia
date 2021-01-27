7bde24d40b9bc8b4f017a51d5e74d0409abb6c44
Luiz Felicio <unifelicio@gmail.com>
2021-01-27 18:12:27 -0300

    refactor(web): add technology types to enum folder

--- packages/web/utils/enums/technology.enums.js
+++ packages/web/utils/enums/technology.enums.js
@@ -9,9 +9,21 @@ export const STATUS = {
 	PUBLISHED: 'published',
 };
 
+export const TYPES = [
+	{ label: 'Equipamento', value: 'equipment' },
+	{ label: 'Material', value: 'material' },
+	{ label: 'Metodologia', value: 'methodology' },
+	{ label: 'Modelo', value: 'model' },
+	{ label: 'Processo', value: 'process' },
+	{ label: 'Serviço', value: 'service' },
+	{ label: 'Software', value: 'software' },
+	{ label: 'Outro', value: 'other' },
+];
+
 export default {
 	WHO_DEVELOP: 'who_develop',
 	WHERE_IS_ALREADY_IMPLEMENTED: 'where_is_already_implemented',
 	WHERE_CAN_BE_APPLIED: 'where_can_be_applied',
 	STATUS,
+	TYPES,
 };
