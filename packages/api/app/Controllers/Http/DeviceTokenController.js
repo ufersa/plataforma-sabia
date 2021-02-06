6fd1f209d9771982442cffab3498a1c6290341c6
Alexandre Pontes <alexandre.adames@gmail.com>
2021-02-05 21:29:34 -0300

    Deletes a  single device or all devices

--- packages/api/app/Controllers/Http/DeviceTokenController.js
+++ packages/api/app/Controllers/Http/DeviceTokenController.js
@@ -1,4 +1,6 @@
 const DeviceToken = use('App/Models/DeviceToken');
+const { errorPayload, errors } = require('../../Utils');
+
 class DeviceTokenController {
 	async store({ auth, request }) {
 		const { device_uuid, device_token } = request.all();
@@ -18,6 +20,44 @@ class DeviceTokenController {
 			.where({ user_id: deviceOwner.id })
 			.first();
 		return deviceToken;
+	}
+
+	async destroy({ auth, params, response, request }) {
+		const { uuid } = params;
+		const deviceOwner = await auth.getUser();
+		const result = await DeviceToken.query()
+			.where({ device_uuid: uuid })
+			.where({ user_id: deviceOwner.id })
+			.delete();
+		if (!result) {
+			return response
+				.status(400)
+				.send(
+					errorPayload(
+						errors.RESOURCE_DELETED_ERROR,
+						request.antl('error.resource.resourceDeletedError'),
+					),
+				);
+		}
+		return response.status(200).send({ success: true });
+	}
+
+	async destroyMany({ auth, response, request }) {
+		const deviceOwner = await auth.getUser();
+		const result = await DeviceToken.query()
+			.where({ user_id: deviceOwner.id })
+			.delete();
+		if (!result) {
+			return response
+				.status(400)
+				.send(
+					errorPayload(
+						errors.RESOURCE_DELETED_ERROR,
+						request.antl('error.resource.resourceDeletedError'),
+					),
+				);
+		}
+		return response.status(200).send({ success: true });
 	}
 }
 
--- packages/api/start/routes/deviceTokens.js
+++ packages/api/start/routes/deviceTokens.js
@@ -7,3 +7,5 @@ Route.post('device-tokens', 'DeviceTokenController.store')
 	.middleware(['auth'])
 	.validator('StoreDeviceToken');
 Route.get('device-tokens/:uuid', 'DeviceTokenController.show').middleware(['auth']);
+Route.delete('device-tokens/:uuid', 'DeviceTokenController.destroy').middleware(['auth']);
+Route.delete('device-tokens', 'DeviceTokenController.destroyMany').middleware(['auth']);
