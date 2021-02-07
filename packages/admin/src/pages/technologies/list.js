be6f7d0fe27efb017d679a2060b4f1fe63ea71f7
NaylsonFerreira <naylsonfsa@gmail.com>
2021-02-07 10:35:15 -0300

    removed hardcoded id

--- packages/admin/src/pages/technologies/list.js
+++ packages/admin/src/pages/technologies/list.js
@@ -9,43 +9,65 @@ import {
 	EditButton,
 	DeleteWithConfirmButton,
 	FunctionField,
+	useQuery,
+	Error,
+	Loading,
 } from 'react-admin';
 
 import { ChipField, ReferenceArrayField, TechnologyFilterBar } from '../../components';
 
-const TechnologiesList = ({ basePath, resource, hasCreate, hasEdit, hasList, hasShow }) => (
-	<List
-		basePath={basePath}
-		resource={resource}
-		hasCreate={hasCreate}
-		hasEdit={hasEdit}
-		hasList={hasList}
-		hasShow={hasShow}
-		perPage={25}
-		filters={<TechnologyFilterBar />}
-	>
-		<Datagrid>
-			<TextField source="id" />
-			<TextField source="title" />
-			<ReferenceArrayField label="Responsibles" reference="users" source="users">
-				<SingleFieldList>
-					<ChipField source="full_name" />
-				</SingleFieldList>
-			</ReferenceArrayField>
-			<FunctionField
-				label="TRL"
-				render={({ terms }) => {
-					const trl = terms?.find(({ taxonomy_id }) => taxonomy_id === 4);
-					return trl?.term;
-				}}
-			/>
-			<TextField source="status" />
-			<DateField source="created_at" />
-			<EditButton />
-			<DeleteWithConfirmButton />
-		</Datagrid>
-	</List>
-);
+const TechnologiesList = ({ basePath, resource, hasCreate, hasEdit, hasList, hasShow }) => {
+	const { loading, error, data: stage } = useQuery({
+		type: 'getOne',
+		resource: 'taxonomies/stage',
+		payload: {
+			id: '',
+			pagination: {
+				page: 1,
+				perPage: 1,
+			},
+			sort: {
+				field: 'id',
+				order: 'asc',
+			},
+		},
+	});
+	if (error) return <Error />;
+	if (loading) return <Loading />;
+	return (
+		<List
+			basePath={basePath}
+			resource={resource}
+			hasCreate={hasCreate}
+			hasEdit={hasEdit}
+			hasList={hasList}
+			hasShow={hasShow}
+			perPage={25}
+			filters={<TechnologyFilterBar />}
+		>
+			<Datagrid>
+				<TextField source="id" />
+				<TextField source="title" />
+				<ReferenceArrayField label="Responsibles" reference="users" source="users">
+					<SingleFieldList>
+						<ChipField source="full_name" />
+					</SingleFieldList>
+				</ReferenceArrayField>
+				<FunctionField
+					label="TRL"
+					render={({ terms }) => {
+						const trl = terms?.find(({ taxonomy_id }) => taxonomy_id === stage.id);
+						return trl?.term;
+					}}
+				/>
+				<TextField source="status" />
+				<DateField source="created_at" />
+				<EditButton />
+				<DeleteWithConfirmButton />
+			</Datagrid>
+		</List>
+	);
+};
 TechnologiesList.propTypes = {
 	resource: PropTypes.string.isRequired,
 	basePath: PropTypes.string.isRequired,
