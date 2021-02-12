d9a0bda63c708745224f582ecab8f87227ecfe8d
NaylsonFerreira <naylsonfsa@gmail.com>
2021-02-12 13:17:57 -0300

    Initial translation

--- packages/admin/src/providers/i18nProvider.js
+++ packages/admin/src/providers/i18nProvider.js
@@ -3,8 +3,52 @@ import polyglotI18nProvider from 'ra-i18n-polyglot';
 import englishMessages from 'ra-language-english';
 import ptBrMessages from 'ra-language-pt-br';
 
+const ptBr = {
+	...ptBrMessages,
+	resources: {
+		users: {
+			name: 'Usuarios',
+			fields: {
+				full_name: 'Nome completo',
+			},
+		},
+		institutions: {
+			name: 'Organizaçoes',
+			fields: {
+				name: 'Nome',
+			},
+		},
+	},
+	sabia: {
+		notification: {
+			send_email_sucess: 'Email enviado com sucesso',
+		},
+	},
+};
+const en = {
+	...englishMessages,
+	resources: {
+		technologies: { name: 'Technologies' },
+		terms: { name: 'Terms' },
+		taxonomies: { name: 'Taxonomies' },
+		users: { name: 'Users' },
+		roles: { name: 'Roles' },
+		permissions: { name: 'Permissions' },
+		reviewers: { name: 'Reviewers' },
+		orders: { name: 'Orders' },
+		institutions: { name: 'Institutions' },
+		ideas: { name: 'Ideas' },
+		announcements: { name: 'Announcements' },
+	},
+	sabia: {
+		notification: {
+			send_email_sucess: 'Email successfully sent',
+		},
+	},
+};
+
 const i18nProvider = polyglotI18nProvider(
-	(locale) => (locale === 'pt-br' ? ptBrMessages : englishMessages),
+	(locale) => (locale === 'pt-br' ? ptBr : en),
 	'en', // Default locale
 );
 
