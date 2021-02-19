fd0cf28106bbc21bfca840362e4262dc89bd40bb
Alexandre Pontes <alexandre.adames@gmail.com>
2021-02-19 15:34:46 -0300

    Fixes error

--- packages/web/components/Header/UserHeader.js
+++ packages/web/components/Header/UserHeader.js
@@ -27,7 +27,7 @@ const UserHeader = () => {
 		<LoginBox ref={ref}>
 			<UserButton type="button" onClick={handleToggleDropDown}>
 				<MdAccountCircle color={colors.secondary} />
-				<span>{user?.first_name.split(' ')[0] || t('common:login')}</span>
+				<span>{user?.first_name || t('common:login')}</span>
 			</UserButton>
 			<UserProfileDropDown visible={isDropDownVisible} toggleVisible={toggleVisible} />
 		</LoginBox>
