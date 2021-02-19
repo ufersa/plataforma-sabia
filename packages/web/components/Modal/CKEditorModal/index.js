cbb7920eb4cdfbeb6908934576f5aeba56c70846
Luiz Felicio <unifelicio@gmail.com>
2021-02-19 16:13:04 -0300

    feat(web): add props to ckeditor modal

--- packages/web/components/Modal/CKEditorModal/index.js
+++ packages/web/components/Modal/CKEditorModal/index.js
@@ -1,21 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import dynamic from 'next/dynamic';
+import styled, { css } from 'styled-components';
+
+import { RectangularButton } from '../../Button';
+import { Spinner } from '../../Loading';
 
 const Editor = dynamic(() => import('../../Editor'), {
 	ssr: false,
+	loading: () => <Spinner />,
 });
 
-const CKEditorModal = ({ form, name, config, onChange, defaultValue, renderWithController }) => {
+const Modal = styled.div`
+	${({ theme: { colors, metrics } }) => css`
+		display: flex;
+		flex-direction: column;
+
+		background-color: ${colors.white};
+		border-radius: ${metrics.baseRadius}rem;
+		padding: 1.2rem;
+
+		max-width: 95vw;
+		width: 600px;
+		.ck-editor__editable_inline {
+			width: 100%;
+			height: 300px;
+		}
+
+		button {
+			margin-top: 1.6rem;
+		}
+	`}
+`;
+
+const CKEditorModal = ({
+	form,
+	name,
+	config,
+	onChange,
+	defaultValue,
+	renderWithController,
+	closeModal,
+}) => {
 	return (
-		<Editor
-			form={form}
-			name={name}
-			config={config}
-			onChange={onChange}
-			defaultValue={defaultValue}
-			renderWithController={renderWithController}
-		/>
+		<Modal>
+			<Editor
+				form={form}
+				name={name}
+				config={config}
+				onChange={onChange}
+				defaultValue={defaultValue}
+				renderWithController={renderWithController}
+			/>
+			<RectangularButton colorVariant="green" variant="filled" onClick={closeModal}>
+				Fechar
+			</RectangularButton>
+		</Modal>
 	);
 };
 
@@ -26,6 +66,7 @@ CKEditorModal.propTypes = {
 	onChange: PropTypes.func,
 	defaultValue: PropTypes.string,
 	renderWithController: PropTypes.bool,
+	closeModal: PropTypes.func.isRequired,
 };
 
 CKEditorModal.defaultProps = {
