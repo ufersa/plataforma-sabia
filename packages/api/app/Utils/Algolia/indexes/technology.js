f1e17ce24f9fb3b2df7d3ccfe1171744e1ddf0fb
Luiz Felicio <unifelicio@gmail.com>
2021-03-03 16:41:25 -0300

    refactor(api): remove technology category from algolia index

--- packages/api/app/Utils/Algolia/indexes/technology.js
+++ packages/api/app/Utils/Algolia/indexes/technology.js
@@ -1,7 +1,6 @@
 const { initIndex } = require('../core');
 const { roles } = require('../../roles_capabilities');
 
-const CATEGORY_TAXONOMY_SLUG = 'CATEGORY';
 const CLASSIFICATION_TAXONOMY_SLUG = 'CLASSIFICATION';
 const DIMENSION_TAXONOMY_SLUG = 'DIMENSION';
 const TARGET_AUDIENCE_TAXONOMY_SLUG = 'TARGET_AUDIENCE';
@@ -17,6 +16,9 @@ const TECHNOLOGY_TYPE = {
 	other: 'Outro',
 };
 
+const defaultTermMasc = 'Não definido';
+const defaultTermFem = 'Não definida';
+
 /**
  * Returns all technology terms related to algolia index
  *
@@ -24,16 +26,12 @@ const TECHNOLOGY_TYPE = {
  * @returns {object} The technology terms to be indexed by algolia
  */
 const normalizeTerms = (technology) => {
-	const defaultTermMasc = 'Não definido';
-	const defaultTermFem = 'Não definida';
-
 	const termsObj = technology.terms.reduce((acc, obj) => {
 		acc[obj.taxonomy.taxonomy] = obj.term;
 		return acc;
 	}, {});
 
 	return {
-		category: termsObj[CATEGORY_TAXONOMY_SLUG] || defaultTermFem,
 		classification: termsObj[CLASSIFICATION_TAXONOMY_SLUG] || defaultTermFem,
 		dimension: termsObj[DIMENSION_TAXONOMY_SLUG] || defaultTermFem,
 		targetAudience: termsObj[TARGET_AUDIENCE_TAXONOMY_SLUG] || defaultTermMasc,
@@ -75,26 +73,19 @@ const normalizeCosts = (technology) => {
  * @returns {object} The technology data for Algolia
  */
 const prepareTechnology = (technology) => {
-	const defaultTermMasc = 'Não definido';
-	const defaultTermFem = 'Não definida';
-
 	const technologyData =
 		typeof technology?.toJSON === 'function' ? technology.toJSON() : technology;
 
 	const technologyForAlgolia = {
 		...technologyData,
-		category: defaultTermFem,
 		classification: defaultTermFem,
 		dimension: defaultTermFem,
 		targetAudience: defaultTermMasc,
 	};
 
 	if (technologyForAlgolia.terms && technologyForAlgolia.terms.length) {
-		const { category, classification, dimension, targetAudience } = normalizeTerms(
-			technologyForAlgolia,
-		);
+		const { classification, dimension, targetAudience } = normalizeTerms(technologyForAlgolia);
 
-		technologyForAlgolia.category = category;
 		technologyForAlgolia.classification = classification;
 		technologyForAlgolia.dimension = dimension;
 		technologyForAlgolia.targetAudience = targetAudience;
