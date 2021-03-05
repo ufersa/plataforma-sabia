28606c4d9d3635106f2ae509c584cacda0601cdd
Alexandre Ádames <alexandre.adames@gmail.com>
2021-03-05 19:09:15 -0300

    Updates emails design and content (#854)
    * Updates global mail layout
    
    * Updates service-requested template
    
    * Updates confirm-account template
    
    * Updates active-account template
    
    * Add min-height to container in mail layout
    
    * Updates forgot-password template
    
    * Updates reset-password template
    
    * Adds Roboto google font family to mail layout
    
    * Improves global mail layout
    
    * Changes svg files to img in mail layout
    
    * Hosts mail layout images in AWS S3
    
    * Updates new-email-verification template
    
    * Updates sucess-change-email template
    
    * Updates technology-invitation template
    
    * Updates approved-reviewer template
    
    * Updates technology-reviewer template
    
    * Updates technology-revision-revoked template
    
    * Updates technology revision templates
    
    * Updates technology-order template
    
    * Updates technology-order-cancelled template
    
    * Updates technology-order-closed template
    
    * Updates technology question templates
    
    * Updates announcement-published template
    
    * Updates contact-us template
    
    * Updates service-requested template
    
    * Removes strong tag to service-requested template
    
    * Revert gitignore changes
    
    * Updates env vars in hooks and templates
    
    * Platform mail as global view var
    
    * Updates contact-us template

--- packages/api/config/upload.js
+++ packages/api/config/upload.js
@@ -5,4 +5,5 @@ module.exports = {
 	allowedFormats: ['jpg', 'jpeg', 'jfif', 'pjpeg', 'pjp', 'png', 'webp', 'pdf'],
 	maxFileSize: '5mb',
 	fileTypes: ['image', 'application'],
+	storageURL: 'https://plataforma-sabia-api-production.s3-sa-east-1.amazonaws.com',
 };
--- packages/api/start/hooks.js
+++ packages/api/start/hooks.js
@@ -29,4 +29,12 @@ hooks.after.providersBooted(() => {
 	Object.values(CustomValidators).forEach((validator) => {
 		Validator.extend([validator.name], validator);
 	});
+
+	const Config = use('Adonis/Src/Config');
+	const View = use('View');
+
+	View.global('APP_URL', Config.get('app.appURL'));
+	View.global('WEB_URL', Config.get('app.webURL'));
+	View.global('STORAGE_URL', Config.get('upload.storageURL'));
+	View.global('PLATFORM_MAIL', Config.get('mail.platform.mail'));
 });
