e96bacca28c9452a0c796e3d7db1a2c2a2c17ceb
Marcus Mota <marcushcsm@gmail.com>
2021-03-15 21:41:13 +0000

    fix redis connection

--- packages/api/config/redis.js
+++ packages/api/config/redis.js
@@ -19,7 +19,7 @@ module.exports = {
 	| Redis connection to be used by default.
 	|
 	*/
-	connection: Env.get('REDIS_HOST', 'local'),
+	connection: Env.get('REDIS_CONNECTION', 'local'),
 
 	/*
 	|--------------------------------------------------------------------------
