668df026af5c23805e487f261837ff19e78c915b
Luiz Felicio <unifelicio@gmail.com>
2021-03-17 20:24:06 -0300

    feat(web): add util for getting service type label

--- packages/web/utils/service.js
+++ packages/web/utils/service.js
@@ -19,3 +19,20 @@ export const typeOptions = [
 	{ label: 'Perícia', value: 'expertise' },
 	{ label: 'Outro', value: 'other' },
 ];
+
+/**
+ * Returns service type label text based on type key
+ *
+ * @param {string} key The service type key
+ * @returns {string} Service type label text
+ */
+export const getTypeLabel = (key) =>
+	({
+		labor: 'Mão-de-obra',
+		specialized_technical_work: 'Trabalho técnico especializado',
+		consulting: 'Consultoria',
+		analysis: 'Análise',
+		examination: 'Exame',
+		expertise: 'Perícia',
+		other: 'Outro',
+	}[key]);
