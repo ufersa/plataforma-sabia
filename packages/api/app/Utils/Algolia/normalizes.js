bf684f1225db5584812abb63791f7ba9c75f051e
Luiz Felicio <unifelicio@gmail.com>
2021-03-18 17:45:21 -0300

    refactor(api): do not use module es6 export

--- packages/api/app/Utils/Algolia/normalizes.js
+++ packages/api/app/Utils/Algolia/normalizes.js
@@ -1,8 +1,11 @@
-/* eslint-disable import/prefer-default-export */
 /**
  * Returns an array of keywords
  *
  * @param {[]} arr The keywords initial array of objects
  * @returns {string[]} The service keywords
  */
-export const normalizeKeywords = (arr) => arr.map((keyword) => keyword?.term);
+const normalizeKeywords = (arr) => arr.map((keyword) => keyword?.term);
+
+module.exports = {
+  normalizeKeywords
+}
