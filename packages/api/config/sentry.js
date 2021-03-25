2c9cd2c840bbb372adce89dcf1d751741c870c71
Mateus Sampaio <mateus4k@protonmail.ch>
2021-03-25 20:24:39 -0300

    update sentry env

--- packages/api/config/sentry.js
+++ packages/api/config/sentry.js
@@ -2,18 +2,15 @@ const Env = use('Env');
 
 module.exports = {
 	/*
-   |--------------------------------------------------------------------------
-   | API key
-   |--------------------------------------------------------------------------
-   */
-	dsn: Env.get(
-		'SENTRY_DSN',
-		'https://e7836d48c6dc49a48da700b59241f1cc@o302563.ingest.sentry.io/5691432',
-	),
+	|--------------------------------------------------------------------------
+	| API key
+	|--------------------------------------------------------------------------
+	*/
+	dsn: Env.get('SENTRY_DSN'),
 
 	environment: Env.get('SENTRY_ENVIRONMENT', Env.get('APP_ENV')),
 
 	options: {
-		// captureUnhandledRejections: true
+		captureUnhandledRejections: true,
 	},
 };
