b1f4f0a5b9ed861259b851ef7cee44f04c014e38
Alexandre Pontes <alexandre.adames@gmail.com>
2021-04-06 22:22:40 -0300

    Updates institution list in user form

---
+++ packages/api/database/seeds/18_InstitutionSeeder.js
@@ -0,0 +1,31 @@
+/*
+|--------------------------------------------------------------------------
+| InstitutionSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory');
+const User = use('App/Models/User');
+
+class InstitutionSeeder {
+	async run() {
+		const getRandom = (list) => {
+			return list.rows[Math.floor(Math.random() * list.rows.length)];
+		};
+		const institutions = await Factory.model('App/Models/Institution').createMany(15);
+		const users = await User.all();
+		await Promise.all(
+			institutions.map(async (institution) => {
+				const user = getRandom(users);
+				await institution.responsible().associate(user);
+			}),
+		);
+	}
+}
+
+module.exports = InstitutionSeeder;
