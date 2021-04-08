9c7b6173176988f3661ec4914e0fd12047af9927
Mateus Sampaio <mateus4k@protonmail.ch>
2021-04-08 17:59:27 -0300

    improve header metatags

--- packages/web/components/head.js
+++ packages/web/components/head.js
@@ -2,35 +2,50 @@ import React from 'react';
 import NextHead from 'next/head';
 import PropTypes from 'prop-types';
 
-const Head = ({ title, description, keywords, url, ogImage, noIndex, children }) => (
-	<NextHead>
-		<meta charSet="UTF-8" />
-		<title>{title || 'Plataforma Sabiá'}</title>
-		<meta
-			name="description"
-			content={
-				description || 'Encontre tecnologias e serviços voltados o semiárido brasileiro'
-			}
-		/>
-		<meta name="keywords" content={Array.isArray(keywords) ? keywords.join(', ') : keywords} />
-		<meta name="viewport" content="width=device-width, initial-scale=1" key="viewport" />
-		<link rel="icon" sizes="192x192" href="/static/touch-icon.png" key="touch-icon" />
-		<link rel="apple-touch-icon" href="/static/apple-touch-icon.png" key="apple-touch-icon" />
-		<link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" key="mask-icon" />
-		<link rel="icon" href="/static/favicon.ico" key="favicon.ico" />
-		<meta property="og:url" content={url} key="og:url" />
-		<meta property="og:title" content={title || ''} key="og:title" />
-		<meta property="og:description" content={description} key="og:description" />
-		<meta name="twitter:site" content={url} />
-		<meta name="twitter:card" content="summary_large_image" />
-		<meta name="twitter:image" content={ogImage} />
-		<meta property="og:image" content={ogImage} key="og:image" />
-		<meta property="og:image:width" content="1200" key="og:image:width" />
-		<meta property="og:image:height" content="630" key="og:image:height" />
-		{!!noIndex && <meta name="robots" content="noindex" />}
-		{children}
-	</NextHead>
-);
+const Head = ({ title, description, keywords, url, ogImage, noIndex, children }) => {
+	const metaTitle = title || 'Plataforma Sabiá';
+	const metaDescription =
+		description || 'Encontre tecnologias e serviços voltados para o semiárido brasileiro';
+	const metaKeywords = Array.isArray(keywords) ? keywords.join(', ') : keywords;
+
+	return (
+		<NextHead>
+			<meta charSet="UTF-8" />
+			<title>{metaTitle}</title>
+			<meta name="title" content={metaTitle} />
+			<meta name="description" content={metaDescription} />
+			<meta name="keywords" content={metaKeywords} />
+			<meta name="viewport" content="width=device-width, initial-scale=1" key="viewport" />
+
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content={url} key="og:url" />
+			<meta property="og:title" content={metaTitle} />
+			<meta property="og:description" content={metaDescription} key="og:description" />
+			<meta property="og:image" content={ogImage} key="og:image" />
+			<meta property="og:image:width" content="1200" key="og:image:width" />
+			<meta property="og:image:height" content="628" key="og:image:height" />
+
+			<meta property="twitter:card" content="summary_large_image" />
+			<meta property="twitter:url" content={url} />
+			<meta property="twitter:title" content={metaTitle} />
+			<meta property="twitter:description" content={metaDescription} />
+			<meta property="twitter:image" content={ogImage} />
+
+			{!!noIndex && <meta name="robots" content="noindex" />}
+
+			<link rel="icon" sizes="192x192" href="/static/touch-icon.png" key="touch-icon" />
+			<link
+				rel="apple-touch-icon"
+				href="/static/apple-touch-icon.png"
+				key="apple-touch-icon"
+			/>
+			<link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" key="mask-icon" />
+			<link rel="icon" href="/static/favicon.ico" key="favicon.ico" />
+
+			{children}
+		</NextHead>
+	);
+};
 
 Head.propTypes = {
 	title: PropTypes.string,
