5523aced78268eaa970f405d48c5271a0e3f36ec
Mateus Sampaio <mateus4k@protonmail.ch>
2021-04-11 17:39:32 -0300

    feat(api): add solutions relationship

--- packages/api/app/Models/Institution.js
+++ packages/api/app/Models/Institution.js
@@ -69,6 +69,14 @@ class Institution extends Model {
 	logo() {
 		return this.belongsTo('App/Models/Upload', 'logo_id');
 	}
+
+	technologies() {
+		return this.manyThrough('App/Models/User', 'technologies');
+	}
+
+	services() {
+		return this.manyThrough('App/Models/User', 'services');
+	}
 }
 
 module.exports = Institution;
