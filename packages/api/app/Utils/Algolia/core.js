76770b1ad6e5ed8be6807791781b9636144e81f8
Mateus Sampaio <mateus4k@protonmail.ch>
2021-04-14 21:17:06 -0300

    API is not calling the right Algolia index on endpoints (#995)
    * fix(algolia): call right index everytime

--- packages/api/app/Utils/Algolia/core.js
+++ packages/api/app/Utils/Algolia/core.js
@@ -2,11 +2,10 @@ const Config = use('Config');
 const AlgoliaSearch = use('App/Services/AlgoliaSearch');
 
 const config = Config.get('algolia');
-const getNestedValue = require('../getNestedValue');
 
 const initIndex = (entity) => {
-	const indexName = getNestedValue(config.indexes, entity) || entity;
-	return AlgoliaSearch.initIndex(indexName);
+	const index = config.indexes[entity];
+	return AlgoliaSearch.initIndex(index?.indexName || index);
 };
 
 module.exports = {
