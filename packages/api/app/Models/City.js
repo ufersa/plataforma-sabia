6df6d54f65a674f678cd65fa42cb431f528c897f
Mateus Sampaio <mateus4k@protonmail.ch>
2021-04-24 17:30:02 -0300

    feat(model): improve model

--- packages/api/app/Models/City.js
+++ packages/api/app/Models/City.js
@@ -2,6 +2,47 @@
 const Model = use('Model');
 
 class City extends Model {
+	static get incrementing() {
+		return false;
+	}
+
+	static get visible() {
+		return ['id', 'name'];
+	}
+
+	/**
+	 * Runs the term query with the provided filters.
+	 *
+	 * @param {object} query The query object.
+	 * @param {object} filters The query filters
+	 *
+	 * @returns {object}
+	 */
+	static scopeWithFilters(query, filters) {
+		if (filters.state) {
+			query.where('title', 'LIKE', `%${filters.title}%`);
+		}
+		if (filters.description) {
+			query.where('description', 'LIKE', `%${filters.description}%`);
+		}
+
+		if (filters.keywords) {
+			const keywordsList = filters?.keywords.split(',') || [];
+			if (keywordsList.length) {
+				query.whereHas('terms', (builder) => {
+					builder.whereIn('id', keywordsList);
+				});
+			}
+		}
+
+		return query;
+	}
+
+	/**
+	 * Every city has a state.
+	 *
+	 * @returns {Model} State Model
+	 */
 	state() {
 		return this.belongsTo('App/Models/State');
 	}
