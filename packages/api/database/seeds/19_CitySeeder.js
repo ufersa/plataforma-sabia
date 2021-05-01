2807906ced1f28781034f175074f8b51028b99d8
Mateus Sampaio <mateus4k@protonmail.ch>
2021-05-01 20:15:53 -0300

    refactor(seed): improve file structure

--- packages/api/database/seeds/19_CitySeeder.js
+++ packages/api/database/seeds/19_CitySeeder.js
@@ -1,21 +1,38 @@
+/** @type {import('@adonisjs/framework/src/Config')} */
+const Config = use('Config');
 /** @type {import('@adonisjs/lucid/src/Lucid/Model')} */
 const City = use('App/Models/City');
 /** @type {import('@adonisjs/lucid/src/Lucid/Model')} */
 const State = use('App/Models/State');
 const fetch = require('node-fetch');
+const mocks = require('../utils/mocks');
 
 class CitySeeder {
 	constructor() {
+		this.environment = Config.get('app.appEnv');
 		this.originUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades/distritos';
+		this.mockedData = {
+			cities: mocks.cities,
+			states: mocks.states,
+		};
 	}
 
-	async run() {
+	async cleanDatabase() {
 		await City.query().delete();
 		await State.query().delete();
+	}
 
-		const districts = await fetch(this.originUrl).then((response) => response.json());
+	async getData() {
+		if (this.environment === 'production') {
+			const data = await fetch(this.originUrl).then((response) => response.json());
+			return this.transform(data);
+		}
 
-		const { states, cities } = districts.reduce(
+		return this.mockedData;
+	}
+
+	transform(data) {
+		return data.reduce(
 			(acc, item) => {
 				const uf = item.municipio['regiao-imediata']['regiao-intermediaria'].UF;
 
@@ -31,7 +48,6 @@ class CitySeeder {
 					acc.cities.push({
 						id: Number(item.municipio.id),
 						state_id: Number(uf.id),
-						state_name: uf.nome,
 						state_initials: uf.sigla,
 						name: item.municipio.nome,
 					});
@@ -41,9 +57,17 @@ class CitySeeder {
 			},
 			{ states: [], cities: [] },
 		);
+	}
 
+	async save({ states, cities }) {
 		await State.query().insert(states);
 		await City.query().insert(cities);
+	}
+
+	async run() {
+		await this.cleanDatabase();
+		const data = await this.getData();
+		await this.save(data);
 	}
 }
 
