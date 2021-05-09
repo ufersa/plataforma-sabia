c2048d29afe7db5bc3f0eff76aeb662df04190a2
Mateus Sampaio <mateus4k@protonmail.ch>
2021-05-09 11:33:25 -0300

    test(state): coverage controller

--- packages/api/test/functional/state.spec.js
+++ packages/api/test/functional/state.spec.js
@@ -3,12 +3,11 @@ const { trait, test } = use('Test/Suite')('State');
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const State = use('App/Models/State');
 
-/** @type {import('@adonisjs/vow/src/ApiClient')} */
 trait('Test/ApiClient');
 trait('Auth/Client');
 trait('DatabaseTransactions');
 
-test.skip('GET /states returns all states', async ({ client, assert }) => {
+test('GET /states returns all states', async ({ client, assert }) => {
 	const response = await client.get('/states').end();
 	const states = (
 		await State.query()
@@ -21,7 +20,7 @@ test.skip('GET /states returns all states', async ({ client, assert }) => {
 	assert.equal(response.body[0].name, states[0].name);
 });
 
-test.skip('GET /states?name=PE returns states with name or initials similar to PE', async ({
+test('GET /states?name=PE returns states with name or initials similar to PE', async ({
 	client,
 	assert,
 }) => {
@@ -44,6 +43,35 @@ test.skip('GET /states?name=PE returns states with name or initials similar to P
 	assert.equal(response.body[0].name, states[0].name);
 });
 
-test('GET /states/:id/cities returns all cities in a given state', async () => {
-	// assert, client
+test('GET /states/:id/cities returns all cities in a given state', async ({ client }) => {
+	const state = (
+		await State.query()
+			.with('cities')
+			.first()
+	).toJSON();
+
+	const response = await client.get(`/states/${state.id}/cities`).end();
+
+	response.assertStatus(200);
+	response.assertJSONSubset(state.cities);
+});
+
+test('GET /states/:id/cities returns cities in a given state filtering by city name', async ({
+	client,
+}) => {
+	const cityInitialName = 'a';
+
+	const state = (
+		await State.query()
+			.with('cities', (query) => query.where('name', 'like', `${cityInitialName}%`))
+			.first()
+	).toJSON();
+
+	const response = await client
+		.get(`/states/${state.id}/cities`)
+		.send({ name: cityInitialName })
+		.end();
+
+	response.assertStatus(200);
+	response.assertJSONSubset(state.cities);
 });
