28f09a86e2801fc12e470720d280015ff8b298a9
Mateus Sampaio <mateus4k@protonmail.ch>
2021-05-14 20:18:30 -0300

    docs(states): both routes

--- packages/api/start/routes/states.js
+++ packages/api/start/routes/states.js
@@ -3,39 +3,94 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
-/** Contact routes */
+/** State routes */
 /**
- * @api {post} /contact Send mail
- * @apiGroup Contact
- * @apiParam {String} name User name
- * @apiParam {String} email User mail
- * @apiParam {String} phone User phone
- * @apiParam {String} subject Mail subject
- * @apiParam {String} message Mail message
- * @apiParamExample  {json} Request sample:
- *    {
- *		"name": "any name",
- *		"email": "any@mail.com",
- *		"phone": "any number",
- *		"subject": "any subject",
- *		"message": "any message"
- *    }
+ * @api {get} /states Gets all states
+ * @apiParam {String} name Optional state name or initials to filter
+ * @apiParamExample {json} Request sample:
+ * /states/?name=rio
+ * @apiGroup States
+ * @apiSuccess {Number} id State id
+ * @apiSuccess {String} name State name
+ * @apiSuccess {String} initials State initials
  * @apiSuccessExample {json} Success
- * HTTP/1.1 204 OK
- * @apiErrorExample {json} Validation Error: Message Required
+ * HTTP/1.1 200 OK
+ * [
+ *   {
+ *     "id": 33,
+ *     "name": "Rio de Janeiro",
+ *     "initials": "RJ"
+ *   },
+ *   {
+ *     "id": 24,
+ *     "name": "Rio Grande do Norte",
+ *     "initials": "RN"
+ *   },
+ *   {
+ *     "id": 43,
+ *     "name": "Rio Grande do Sul",
+ *     "initials": "RS"
+ *   }
+ * ]
+ * @apiError (Bad Request 400) {Object} error Error object
+ * @apiError (Bad Request 400) {String} error.error_code Error code
+ * @apiError (Bad Request 400) {String} error.message Error message
+ * @apiErrorExample {json} Validation Error: Maximum name length
  * HTTP/1.1 400 Bad Request
  * {
  *   "error": {
  *     "error_code": "VALIDATION_ERROR",
  *     "message": [
  *       {
- *         "message": "The message is required.",
- *         "field": "message",
- *         "validation": "required"
+ *         "message": "The name should not be more than 30.",
+ *         "field": "name",
+ *         "validation": "max"
  *       }
  *     ]
  *   }
  * }
  */
 Route.get('states', 'StateController.index').validator('GetState');
+
+/**
+ * @api {get} /states/:id/cities Gets all cities in a given state
+ * @apiGroup States
+ * @apiParam (Route Param) {Number} id Mandatory State ID
+ * @apiParamExample {json} Request sample:
+ * /states/43/cities
+ * @apiParam {String} name Optional city name to filter
+ * @apiParamExample {json} Request sample:
+ * /states/43/cities?name=gramado
+ * @apiSuccess {Number} id City id
+ * @apiSuccess {String} name City name
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * [
+ *   {
+ *     "id": 4300034,
+ *     "name": "Aceguá"
+ *   },
+ *   {
+ *     "id": 4300059,
+ *     "name": "Água Santa"
+ *   }
+ * ]
+ * @apiError (Bad Request 400) {Object} error Error object
+ * @apiError (Bad Request 400) {String} error.error_code Error code
+ * @apiError (Bad Request 400) {String} error.message Error message
+ * @apiErrorExample {json} Validation Error: Maximum name length
+ * HTTP/1.1 400 Bad Request
+ * {
+ *   "error": {
+ *     "error_code": "VALIDATION_ERROR",
+ *     "message": [
+ *       {
+ *         "message": "The name should not be more than 50.",
+ *         "field": "name",
+ *         "validation": "max"
+ *       }
+ *     ]
+ *   }
+ * }
+ */
 Route.get('states/:id/cities', 'StateController.cities').validator('GetCity');
