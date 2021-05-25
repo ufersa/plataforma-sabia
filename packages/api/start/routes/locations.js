1d512a18312b2444aac9551ae41bb11d86d3aa85
Alexandre Pontes <alexandre.adames@gmail.com>
2021-05-25 18:30:35 -0300

    Routes: Show, Update e Delete

--- packages/api/start/routes/locations.js
+++ packages/api/start/routes/locations.js
@@ -2,6 +2,14 @@ const Route = use('Route');
 
 Route.get('locations', 'LocationController.index').middleware(['handleParams']);
 
+Route.get('locations/:id', 'LocationController.show').middleware(['handleParams']);
+
 Route.post('locations', 'LocationController.store')
 	.middleware(['auth'])
 	.validator('StoreLocation');
+
+Route.put('locations/:id', 'LocationController.update')
+	.middleware(['auth'])
+	.validator('UpdateLocation');
+
+Route.delete('locations/:id', 'LocationController.destroy').middleware(['auth']);
