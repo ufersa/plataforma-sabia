7bbf6ee3a7d524a98b82e6234adeef8949ab977f
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-06-18 15:35:48 -0300

    feat(web): create servce to delete a location

--- packages/web/services/locations.js
+++ packages/web/services/locations.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-import { apiPost } from './api';
+import { apiDelete, apiPost } from './api';
 
 /**
  * Creates a new location
@@ -11,6 +11,26 @@ export const createLocation = async (data = {}) => {
 	const response = await apiPost(`locations`, { ...data });
 
 	if (response.status !== 201 && response.status !== 200) {
+		return { error: true, messages: response.data?.error?.message || [] };
+	}
+
+	return response.data;
+};
+
+/**
+ * Deletes a location
+ *
+ * @param {string|number} id The location id
+ * @returns {object} Success or not
+ */
+export const deleteLocation = async (id) => {
+	if (!id) {
+		throw new Error('Missing parameter `id` at updateTechnologyLocations.');
+	}
+
+	const response = await apiDelete(`locations/${id}`);
+
+	if (response.status !== 200) {
 		return { error: true, messages: response.data?.error?.message || [] };
 	}
 
