68b79bca0b7cecbd3abb9e69274d3ac35a2c3a27
Alexandre Ádames <alexandre.adames@gmail.com>
2021-06-22 18:57:55 -0300

    Retrieves location with passing existent place_id (#1079)

--- packages/api/app/Validators/StoreLocation.js
+++ packages/api/app/Validators/StoreLocation.js
@@ -3,7 +3,7 @@ const BaseValidator = use('App/Validators/BaseValidator');
 class StoreLocation extends BaseValidator {
 	get rules() {
 		return {
-			place_id: 'string|unique:locations',
+			place_id: 'string',
 			address: 'required|string',
 			city_id: 'number|exists:cities,id',
 			lat: 'required|string',
