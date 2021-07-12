5c2153a6d7e321e070d2b332425354f60632b713
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-07-12 18:08:43 -0300

    test(web): add coverage for user profile dropdown requests

--- packages/web/components/UserProfile/__tests__/UserProfileDropDown.js
+++ packages/web/components/UserProfile/__tests__/UserProfileDropDown.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { render, screen } from 'test-utils';
+import { render, screen, act } from 'test-utils';
+import { cache } from 'swr';
 import UserProfileDropDown from '../UserProfileDropDown';
+import * as useAuth from '../../../hooks/useAuth';
+import * as userServices from '../../../services/user';
 
 describe('UserProfileDropDown', () => {
+	afterEach(() => {
+		cache.clear();
+	});
+
 	test('it renders as expected if visible is true', () => {
 		const { container } = render(<UserProfileDropDown visible toggleVisible={() => {}} />);
 
@@ -19,5 +26,39 @@ describe('UserProfileDropDown', () => {
 		expect(screen.queryAllByRole('list')).toHaveLength(0);
 		expect(screen.queryByRole('button', { name: /sair|logout/i })).toBeNull();
 		expect(container).toMatchSnapshot();
+	});
+
+	it('should not request notifications if user is unauthenticated', () => {
+		jest.spyOn(useAuth, 'default').mockReturnValue({
+			user: {},
+			isAuthenticated: false,
+		});
+		const mockedQuestions = jest.spyOn(userServices, 'getUserUnansweredQuestions');
+		const mockedMessages = jest.spyOn(userServices, 'getUserNewMessages');
+
+		render(<UserProfileDropDown visible={false} toggleVisible={() => {}} />);
+
+		expect(mockedQuestions).toHaveBeenCalledTimes(0);
+		expect(mockedMessages).toHaveBeenCalledTimes(0);
+	});
+
+	it('should request notifications if user is authenticated', async () => {
+		jest.spyOn(useAuth, 'default').mockReturnValue({
+			user: {
+				email: 'test@email.com',
+			},
+			isAuthenticated: true,
+		});
+		const mockedQuestions = jest.spyOn(userServices, 'getUserUnansweredQuestions');
+		const mockedMessages = jest.spyOn(userServices, 'getUserNewMessages');
+
+		act(() => {
+			render(<UserProfileDropDown visible toggleVisible={() => {}} />);
+		});
+
+		// This expect is only for waiting the new render of SWR so we avoid ACT warnings
+		expect(await screen.findByRole('button')).toBeInTheDocument();
+		expect(mockedQuestions).toHaveBeenCalledTimes(1);
+		expect(mockedMessages).toHaveBeenCalledTimes(1);
 	});
 });
