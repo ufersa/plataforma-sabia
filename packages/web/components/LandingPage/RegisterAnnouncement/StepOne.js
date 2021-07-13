524abb071d0646c940cab407a83237ff9c6c1d9e
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-07-13 10:50:24 -0300

    feat(web): use async react-select in announcements page

--- packages/web/components/LandingPage/RegisterAnnouncement/StepOne.js
+++ packages/web/components/LandingPage/RegisterAnnouncement/StepOne.js
@@ -1,38 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import useSWR from 'swr';
+import debounce from 'lodash.debounce';
 import { getInstitutions } from '../../../services';
 import { getInstitutionLabel } from '../../../utils/helper';
 import { InputField, SelectField, TextField } from '../../Form';
 import { inputWrapperCss } from './styles';
 import { useAuth } from '../../../hooks';
 
+const mapInstitutionsOptions = (institutions) =>
+	institutions.map((institution) => ({
+		label: getInstitutionLabel(institution),
+		value: institution.id,
+	}));
+
 const StepOne = ({ form }) => {
 	const { isAuthenticated } = useAuth();
 
-	const { data: { data: institutions } = {}, isValidating: isValidatingInstitutions } = useSWR(
+	const { data: { data: institutions } = {} } = useSWR(
 		isAuthenticated ? 'get-institutions' : null,
-		() => getInstitutions({ perPage: 50, order: 'desc' }),
+		() => getInstitutions({ perPage: 10, order: 'desc' }),
 		{
 			revalidateOnFocus: false,
 		},
 	);
 
+	const handleFetchOptions = debounce((value, callback) => {
+		getInstitutions({ filterBy: 'name', filter: value, order: 'desc' }).then((response) => {
+			const mappedOptions = mapInstitutionsOptions(response.data);
+			callback(mappedOptions);
+		});
+	}, 300);
+
 	return (
 		<>
 			<SelectField
 				form={form}
-				isSearchable
 				name="institution_id"
 				label="Organização"
-				placeholder="Escolha a organização"
-				isLoading={isValidatingInstitutions}
-				options={institutions?.map((institution) => ({
-					label: getInstitutionLabel(institution),
-					value: institution.id,
-				}))}
+				placeholder="Pesquise a organização"
 				variant="rounded"
 				validation={{ required: true }}
+				isAsync
+				cacheOptions
+				defaultOptions={institutions ? mapInstitutionsOptions(institutions) : []}
+				loadOptions={handleFetchOptions}
+				loadingMessage={() => 'Carregando...'}
+				noOptionsMessage={() => 'Nenhuma organização encontrada...'}
+				instanceId="select-institutions-announcements"
 			/>
 			<InputField
 				form={form}
