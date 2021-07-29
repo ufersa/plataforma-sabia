0a169c7d541cc42549f7b462d0275850b1017eaa
Mateus Sampaio <mateus4k@protonmail.ch>
2021-07-29 12:25:07 -0300

    get technologies count by date

--- packages/api/app/Jobs/CollectBusinessMetrics.js
+++ packages/api/app/Jobs/CollectBusinessMetrics.js
@@ -7,10 +7,15 @@ class CollectBusinessMetrics {
 	constructor() {
 		this.prometheus = prometheus;
 		this.metrics = {
-			technologiesStatus: {
+			technologiesStatusCount: {
 				name: 'technologies_statuses_count',
 				help: 'Number of technologies grouped by status',
-				labelNames: ['type'],
+				labelNames: ['status'],
+			},
+			technologiesDateCount: {
+				name: 'technologies_date_count',
+				help: 'Number of technologies grouped by date',
+				labelNames: ['date'],
 			},
 		};
 	}
@@ -23,27 +28,52 @@ class CollectBusinessMetrics {
 		// eslint-disable-next-line no-console
 		console.log(`Date => ${new Date().toJSON()}`);
 
-		// const publishedTechnologiesCount = await Database.table('technologies')
-		// 	.where('status', 'published')
-		// 	.getCount();
-
-		// new this.prometheus.Gauge(this.metrics.publishedTechnologies).set(
-		// 	publishedTechnologiesCount,
-		// );
-
 		// Published Technologies Metric
 		{
-			this.prometheus.register.removeSingleMetric(this.metrics.technologiesStatus.name);
+			this.prometheus.register.removeSingleMetric(this.metrics.technologiesStatusCount.name);
 
 			const count = await Database.raw(
 				'SELECT status, count(*) as count from technologies GROUP BY status',
 			);
 
-			const statusGauge = new this.prometheus.Gauge(this.metrics.technologiesStatus);
+			const gauge = new this.prometheus.Gauge(this.metrics.technologiesStatusCount);
 
 			count[0].forEach((item) => {
-				statusGauge.set({ type: item.status }, item.count);
+				gauge.set({ status: item.status }, item.count);
 			});
+		}
+
+		// Technologies Count by Date
+		{
+			this.prometheus.register.removeSingleMetric(this.metrics.technologiesDateCount.name);
+
+			const count = await Database.raw(
+				'SELECT DATE_FORMAT(created_at,"%Y-%m-%d") as date, count(*) as count from technologies GROUP BY DATE_FORMAT(created_at,"%Y-%m-%d") ORDER BY DATE_FORMAT(created_at,"%Y-%m-%d") DESC',
+			);
+
+			const counter = new this.prometheus.Counter(this.metrics.technologiesDateCount);
+
+			count[0].forEach((item) => {
+				counter.inc({ date: item.date }, item.count);
+			});
+		}
+
+		// Technologies Count by Date
+		{
+			// this.prometheus.register.removeSingleMetric(this.metrics.technologiesDateCount.name);
+
+			const count = await Database.table('technologies')
+				.join('institutions')
+				.groupBy('institution_id');
+
+			// eslint-disable-next-line no-console
+			console.log(count);
+
+			// const counter = new this.prometheus.Counter(this.metrics.technologiesDateCount);
+
+			// count[0].forEach((item) => {
+			// counter.inc({ date: item.date }, item.count);
+			// });
 		}
 
 		// eslint-disable-next-line no-console
