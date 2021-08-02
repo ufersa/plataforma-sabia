d0eb1ce815f2252598f9954f80accc850ff3053a
Mateus Sampaio <mateus4k@protonmail.ch>
2021-08-02 19:19:29 -0300

    restore flag

--- packages/web/pages/user/my-account/index.js
+++ packages/web/pages/user/my-account/index.js
@@ -402,7 +402,7 @@ const CommonDataForm = ({ form, user, message, loading }) => {
 								placeholder="Pesquise sua instituição"
 								variant="gray"
 								isAsync
-								// cacheOptions
+								cacheOptions
 								defaultOptions={getDefaultInstitutionsOptions()}
 								loadOptions={handleFetchInstitutions}
 								loadingMessage={() => 'Carregando...'}
