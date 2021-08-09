55cf8aa3dff4d32e1bfae04f52ec2e5e9d3ce07a
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-08-09 17:10:49 -0300

    feat(web): add variant and noPadding prop to checkboxfield, improve component code

--- packages/web/components/Form/CheckBoxField.js
+++ packages/web/components/Form/CheckBoxField.js
@@ -1,40 +1,83 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const StyledCheckBox = styled.div`
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	padding: 1rem;
-	font: 1.2rem;
+const StyledCheckBoxMark = styled.span`
+	${({ theme: { colors } }) => css`
+		display: inline-block;
+		position: relative;
+		margin: -1px 0px 0 0;
+		margin-right: 8px;
+		vertical-align: middle;
+		background: ${colors.white};
+		border: 1px solid ${colors.mediumGray};
+		border-radius: 0.2rem;
+		cursor: pointer;
+	`}
 `;
 
 const StyledCheckBoxInput = styled.input`
-	margin: 0rem;
-	width: 1px;
+	width: 0px;
+	height: 0px;
 	position: relative;
-	left: 2.5rem;
-	z-index: -1;
+`;
 
-	&:checked + span {
-		background: ${({ theme }) => theme.colors.blue} -19px top no-repeat;
-		border-color: ${({ theme }) => theme.colors.blue};
-
-		&:before {
-			content: '';
-			position: absolute;
-			top: 16%;
-			right: 34%;
-			display: inline-block;
-			transform: rotate(45deg);
-			height: 12px;
-			width: 7px;
-			border-bottom: 3px solid ${({ theme }) => theme.colors.white};
-			border-right: 3px solid ${({ theme }) => theme.colors.white};
+const checkboxVariants = {
+	default: ({ colors }) => css`
+		${StyledCheckBoxMark} {
+			width: 2.5rem;
+			height: 2.5rem;
 		}
-	}
+
+		& ${StyledCheckBoxInput}:checked + ${StyledCheckBoxMark}:before {
+			top: 16%;
+			height: 1.2rem;
+			width: 0.7rem;
+			border-bottom: 3px solid ${colors.white};
+			border-right: 3px solid ${colors.white};
+		}
+	`,
+	rounded: ({ colors }) => css`
+		${StyledCheckBoxMark} {
+			width: 2rem;
+			height: 2rem;
+		}
+
+		& ${StyledCheckBoxInput}:checked + ${StyledCheckBoxMark}:before {
+			top: 15%;
+			height: 0.9rem;
+			width: 0.6rem;
+			border-bottom: 2px solid ${colors.white};
+			border-right: 2px solid ${colors.white};
+		}
+	`,
+};
+
+const StyledCheckBox = styled.div`
+	${({ theme: { colors }, noPadding, variant }) => css`
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		padding: ${!noPadding ? '1rem' : '0'};
+		font: 1.2rem;
+
+		& ${StyledCheckBoxInput}:checked + ${StyledCheckBoxMark} {
+			background: ${colors.blue} -19px top no-repeat;
+			border-color: ${colors.blue};
+
+			&:before {
+				content: '';
+				position: absolute;
+				top: 16%;
+				right: 34%;
+				display: inline-block;
+				transform: rotate(45deg);
+			}
+		}
+
+		${checkboxVariants[variant]({ colors })}
+	`}
 `;
 
 const StyledCheckBoxLabel = styled.label`
@@ -53,25 +96,10 @@ const StyledCheckBoxLabel = styled.label`
 	}
 `;
 
-const StyledCheckBoxMark = styled.span`
-	display: inline-block;
-	position: relative;
-	top: -1px;
-	width: 2.5rem;
-	height: 2.5rem;
-	margin: -1px 0px 0 0;
-	margin-right: 8px;
-	vertical-align: middle;
-	background: ${({ theme }) => theme.colors.white};
-	border: 1px solid ${({ theme: { colors } }) => colors.mediumGray};
-	border-radius: 0.2rem;
-	cursor: pointer;
-`;
-
-const CheckBoxField = ({ name, value, label, required, onChange, ...rest }) => {
+const CheckBoxField = ({ name, value, label, required, onChange, noPadding, variant, ...rest }) => {
 	const handleOnChange = () => onChange((oldValue) => !oldValue);
 	return (
-		<StyledCheckBox>
+		<StyledCheckBox noPadding={noPadding} variant={variant}>
 			<StyledCheckBoxInput
 				id={name}
 				name={name}
@@ -101,6 +129,8 @@ CheckBoxField.propTypes = {
 	label: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
 	required: PropTypes.bool,
 	onChange: PropTypes.func,
+	noPadding: PropTypes.bool,
+	variant: PropTypes.oneOf(['default', 'rounded']),
 };
 
 CheckBoxField.defaultProps = {
@@ -108,6 +138,8 @@ CheckBoxField.defaultProps = {
 	label: '',
 	required: false,
 	onChange: () => {},
+	noPadding: false,
+	variant: 'default',
 };
 
 export default CheckBoxField;
