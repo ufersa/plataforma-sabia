da931e5e86ea91fa93e8f7305785848ee2ee3718
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-08-16 19:40:01 -0300

    refactor(web): improve last step layout in desktop mode

--- packages/web/components/Register/StepFive/index.js
+++ packages/web/components/Register/StepFive/index.js
@@ -1,27 +1,46 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import Image from 'next/image';
+import { FiArrowRight } from 'react-icons/fi';
 
 import { RectangularButton } from '../../Button';
 
-import { Actions } from '../styles';
 import * as S from './styles';
 
 const StepFive = ({ activeStep }) => {
 	return (
 		<>
-			<S.FinishContainer>
-				<img src="/logo.svg" alt="Plataforma Sabiá" />
+			<S.FinishWrapper>
+				<S.FinishContainer>
+					<S.LogoWrapper>
+						<Image
+							src="/logo.svg"
+							alt="Plataforma Sabiá"
+							width={165}
+							height={48}
+							layout="responsive"
+						/>
+					</S.LogoWrapper>
 
-				<S.FinishTitle>{activeStep.title}</S.FinishTitle>
-				<S.FinishSubtitle>{activeStep.subtitle}</S.FinishSubtitle>
-			</S.FinishContainer>
-			<Actions>
+					<S.FinishTitle>{activeStep.title}</S.FinishTitle>
+					<S.FinishSubtitle>{activeStep.subtitle}</S.FinishSubtitle>
+
+					<Link href="/" passHref>
+						<RectangularButton as="a" variant="round" colorVariant="green">
+							Ir para a página principal
+							<FiArrowRight fontSize="2rem" />
+						</RectangularButton>
+					</Link>
+				</S.FinishContainer>
+			</S.FinishWrapper>
+
+			<S.Actions>
 				<Link href="/" passHref>
 					<RectangularButton as="a" variant="round" colorVariant="orange">
 						Finalizar
 					</RectangularButton>
 				</Link>
-			</Actions>
+			</S.Actions>
 		</>
 	);
 };
--- packages/web/components/Register/StepFive/styles.js
+++ packages/web/components/Register/StepFive/styles.js
@@ -1,26 +1,81 @@
 import styled, { css } from 'styled-components';
+import { Actions as RootActions } from '../styles';
+
+export const FinishWrapper = styled.div`
+	${({ theme: { screens } }) => css`
+		height: 100%;
+
+		@media screen and (min-width: ${screens.medium + 1}px) {
+			background-image: url(/celebration-rafiki.svg);
+			background-repeat: no-repeat;
+			background-position: center right;
+		}
+	`}
+`;
 
 export const FinishContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: space-evenly;
-	height: 100%;
-	text-align: center;
+	${({ theme: { screens } }) => css`
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		max-width: 47rem;
+
+		@media screen and (max-width: ${screens.medium - 1}px) {
+			align-items: center;
+			justify-content: space-evenly;
+			text-align: center;
+
+			a {
+				display: none;
+			}
+		}
+	`}
+`;
+
+export const LogoWrapper = styled.div`
+	${({ theme: { screens } }) => css`
+		width: 100%;
+		margin: 0 auto;
+		max-width: 18.5rem;
+
+		@media screen and (min-width: ${screens.medium + 1}px) {
+			margin: 0;
+			margin-bottom: 8.6rem;
+			max-width: 16.5rem;
+		}
+	`}
 `;
 
 export const FinishTitle = styled.h3`
-	${({ theme: { colors } }) => css`
+	${({ theme: { colors, screens } }) => css`
 		color: ${colors.secondary};
 		max-width: 27.5rem;
 		line-height: 3rem;
+
+		@media screen and (min-width: ${screens.medium + 1}px) {
+			max-width: unset;
+			font-size: 3.6rem;
+			margin-bottom: 3.2rem;
+		}
 	`}
 `;
 
 export const FinishSubtitle = styled.p`
-	${({ theme: { colors } }) => css`
+	${({ theme: { colors, screens } }) => css`
 		color: ${colors.silver};
 		font-size: 1.6rem;
 		line-height: 2.4rem;
+
+		@media screen and (min-width: ${screens.medium + 1}px) {
+			margin-bottom: 3.2rem;
+		}
+	`}
+`;
+
+export const Actions = styled(RootActions)`
+	${({ theme: { screens } }) => css`
+		@media screen and (min-width: ${screens.medium + 1}px) {
+			display: none;
+		}
 	`}
 `;
