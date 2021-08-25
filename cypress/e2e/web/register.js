ab68a957a2bd6d11daa00c2f463a67e9c7ebd6b3
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-08-25 17:24:47 -0300

    test(e2e): add edge case to register test

--- cypress/e2e/web/register.js
+++ cypress/e2e/web/register.js
@@ -58,35 +58,51 @@ describe('User register', () => {
 		cy.findByRole('heading', { name: /validação/i }).should('be.visible');
 	});
 
-	it('should be able to verify account by direct access through confirm account page', () => {
-		cy.intercept('POST', '**/auth/confirm-account').as('confirmAccount');
+	[
+		{
+			key:
+				'should be able to request a new token and confirm account when direct access through confirm account page',
+			requestNewToken: true,
+		},
+		{
+			key:
+				'should be able to confirm account when direct access through confirm account page',
+			requestNewToken: false,
+		},
+	].forEach((test) =>
+		it(test.key, () => {
+			cy.intercept('POST', '**/auth/confirm-account').as('confirmAccount');
 
-		const { email, password } = getRandomUser();
-		cy.visit('/cadastrar').register({ email, password });
-		cy.findByRole('link', { href: '/' }).click();
+			const { email, password } = getRandomUser();
+			cy.visit('/cadastrar').register({ email, password });
+			cy.findByRole('link', { href: '/' }).click();
 
-		cy.visit(`/confirmar-conta/`);
-		cy.resetReceivedEmails();
-		cy.inputType(/e-mail/i, email);
-		cy.findByRole('button', { name: /enviar novamente/i }).click();
+			cy.visit(`/confirmar-conta/`);
+			cy.resetReceivedEmails();
+			cy.inputType(/e-mail/i, email);
 
-		recurse(
-			() => cy.task('getLastEmail', email),
-			(_email) => !!_email,
-			{
-				log: false,
-				delay: 1000,
-				timeout: 20000,
-			},
-		)
-			.then(cy.wrap)
-			.invoke('match', /(?<code>\w+) Siga-nos/i)
-			.its('groups.code')
-			.then((verificationCode) => {
-				cy.inputType(/Verification code input number 1/i, verificationCode);
-				cy.wait('@confirmAccount')
-					.its('response.statusCode')
-					.should('eq', 200);
-			});
-	});
+			if (test.requestNewToken) {
+				cy.findByRole('button', { name: /enviar novamente/i }).click();
+			}
+
+			recurse(
+				() => cy.task('getLastEmail', email),
+				(_email) => !!_email,
+				{
+					log: false,
+					delay: 1000,
+					timeout: 20000,
+				},
+			)
+				.then(cy.wrap)
+				.invoke('match', /(?<code>\w+) Siga-nos/i)
+				.its('groups.code')
+				.then((verificationCode) => {
+					cy.inputType(/Verification code input number 1/i, verificationCode);
+					cy.wait('@confirmAccount')
+						.its('response.statusCode')
+						.should('eq', 200);
+				});
+		}),
+	);
 });
