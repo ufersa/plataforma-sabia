37c4d406403a31b303aed13549e1431b8abf73a5
Mateus Sampaio <mateus4k@protonmail.ch>
2021-08-26 12:39:56 -0300

    Improve security on mail confirmation endpoint (#1164)
    * remove user existance validation
    
    * resend mail only if user exists and is verified

--- packages/api/app/Validators/ResendConfirmationEmail.js
+++ packages/api/app/Validators/ResendConfirmationEmail.js
@@ -3,7 +3,7 @@ const BaseValidator = use('App/Validators/BaseValidator');
 class ResendConfirmationEmail extends BaseValidator {
 	get rules() {
 		return {
-			email: 'required|email|exists:users,email',
+			email: 'required|email',
 		};
 	}
 }
