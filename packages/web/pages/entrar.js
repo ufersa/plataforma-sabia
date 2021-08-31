75bfc03a7b9768bc281d8b395911889b6f990879
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-08-31 14:59:41 -0300

    feat(web): add new login page

--- packages/web/pages/entrar.js
+++ packages/web/pages/entrar.js
@@ -1,7 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import SignInPage from '../screens/SignIn';
 
-const SignIn = () => {
-	return <SignInPage />;
+const SignIn = (props) => {
+	return <SignInPage {...props} />;
+};
+
+SignIn.getInitialProps = ({ query }) => {
+	return {
+		redirect: query.redirect,
+	};
 };
 
 export default SignIn;
