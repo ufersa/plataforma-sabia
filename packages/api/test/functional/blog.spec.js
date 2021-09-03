56b7a1a756e7000c6a5aa7ba2407a1479b4d722c
Mateus Sampaio <mateus4k@protonmail.ch>
2021-09-03 09:14:41 -0300

    coverage e2e

---
+++ packages/api/test/functional/blog.spec.js
@@ -0,0 +1,39 @@
+const { test, trait, beforeEach } = use('Test/Suite')('Contact');
+const Blog = use('Blog');
+
+trait('Test/ApiClient');
+
+beforeEach(async () => {
+	Blog.sandbox.reset();
+});
+
+test('GET /blog/posts return blog posts', async ({ client, assert }) => {
+	const response = await client.get('/blog/posts').end();
+
+	response.assertStatus(200);
+	assert.isTrue(Blog.getPosts.calledOnce);
+});
+
+test('GET /blog/posts return blog posts limited by query', async ({ client, assert }) => {
+	const payload = { limit: 3 };
+
+	const response = await client
+		.get('/blog/posts')
+		.send(payload)
+		.end();
+
+	response.assertStatus(200);
+	assert.isTrue(Blog.getPosts.withArgs({ limit: payload.limit }).calledOnce);
+});
+
+test('GET /blog/posts return posts limited by query', async ({ client, assert }) => {
+	const payload = { limit: 3 };
+
+	const response = await client
+		.get('/blog/posts')
+		.send(payload)
+		.end();
+
+	response.assertStatus(200);
+	assert.isTrue(Blog.getPosts.withArgs({ limit: payload.limit }).calledOnce);
+});
